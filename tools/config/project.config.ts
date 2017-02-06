e92a9c81ebe9fe67df1f460ec0c37700f30f9cc3
Rick Zakharov <rick.zakharov@gmail.com>
2017-02-06 15:35:57 -0500

    Adding bootstrap theme, font-awesome libs

--- tools/config/project.config.ts
+++ tools/config/project.config.ts
@@ -24,15 +24,18 @@ export class ProjectConfig extends SeedConfig {
     // Add `NPM` third-party libraries to be injected/bundled.
     this.NPM_DEPENDENCIES = [
       ...this.NPM_DEPENDENCIES,
-      // {src: 'jquery/dist/jquery.min.js', inject: 'libs'},
+      {src: 'jquery/dist/jquery.slim.min.js', inject: 'libs'},
       // {src: 'lodash/lodash.min.js', inject: 'libs'},
+      {src: 'bootstrap/dist/js/bootstrap.min.js', inject: 'libs'},
+      // {src: 'font-awesome/css/font-awesome.min.css', inject: true},
+      {src: 'bootstrap/dist/css/bootstrap.min.css', inject: true},
     ];
 
     // Add `local` third-party libraries to be injected/bundled.
     this.APP_ASSETS = [
       ...this.APP_ASSETS,
       // {src: `${this.APP_SRC}/your-path-to-lib/libs/jquery-ui.js`, inject: true, vendor: false}
-      // {src: `${this.CSS_SRC}/path-to-lib/test-lib.css`, inject: true, vendor: false},
+      {src: `${this.CSS_SRC}/theme/style.min.css`, inject: true, vendor: false},
     ];
 
     // Add packages (e.g. ng2-translate)
