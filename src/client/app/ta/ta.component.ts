fac988e5f5e8ba49fb0c5a7068feaad68ec2a2d0
Subhash Vallala <sgvallala@gmail.com>
2017-02-08 15:09:08 -0500

    Adding basic treatment arm (ta) files

---
+++ src/client/app/ta/ta.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+/**
+ * This class represents the lazy loaded TAComponent.
+ */
+@Component({
+  moduleId: module.id,
+  selector: 'sd-ta',
+  templateUrl: 'ta.component.html',
+  styleUrls: ['ta.component.css']
+})
+export class TAComponent { }
