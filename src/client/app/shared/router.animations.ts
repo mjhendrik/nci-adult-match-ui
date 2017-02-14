1959cb9cfed05e18bb246438e9fca6c4f2dc897f
Rick Zakharov <rick.zakharov@gmail.com>
2017-02-14 16:04:32 -0500

    Fixed scrolling

--- src/client/app/shared/router.animations.ts
+++ src/client/app/shared/router.animations.ts
@@ -6,15 +6,9 @@ export function routerTransition() {
 
 function fadeIn() {
   return trigger('routerTransition', [
-    state('void', style({ position: 'fixed', width: '100%' })),
-    state('*', style({ position: 'fixed', width: '100%' })),
     transition(':enter', [
       style({ opacity: 0 }),
       animate('0.2s ease-in', style({ opacity: 1 }))
-    ]),
-    transition(':leave', [
-      style({ opacity: 1 }),
-      animate('0.2s ease-out', style({ opacity: 0 }))
     ])
   ]);
 }
