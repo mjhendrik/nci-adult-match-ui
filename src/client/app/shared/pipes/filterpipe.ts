3289100b4bbe015980e91e5dda542428684117b8
Subhash Vallala <sgvallala@gmail.com>
2017-02-16 09:25:04 -0500

    Added filterpipe and orderBy pipes

---
+++ src/client/app/shared/pipes/filterpipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform, Injectable } from '@angular/core';
+@Pipe({
+    name: 'filterByText'
+})
+export class filterByText implements PipeTransform {
+    transform(items: any[], filterText: string): any[] {
+        var keys=Object.keys(items[0]);
+        return items.filter(item => {
+            return Object.keys(item).some(key => item[key].toString().toLowerCase().includes(filterText.toLowerCase()));
+        }
+        )
+    }
+}
