796c8d5b3ed74c2700d7b59e5c02f4e256c56708
Subhash Vallala <sgvallala@gmail.com>
2017-02-16 09:25:04 -0500

    Added filterpipe and orderBy pipes

--- src/client/app/dashboard/dashboard.module.ts
+++ src/client/app/dashboard/dashboard.module.ts
@@ -4,10 +4,13 @@ import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { NameListService } from '../shared/name-list/name-list.service';
+import { filterByText } from '../shared/pipes/filterpipe';
+import { OrderBy } from '../shared/pipes/orderBy';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule, DashboardRoutingModule, SharedModule],
-  declarations: [DashboardComponent],
+  imports: [CommonModule, DashboardRoutingModule, SharedModule, FormsModule],
+  declarations: [DashboardComponent, filterByText, OrderBy],
   exports: [DashboardComponent],
   providers: [NameListService]
 })
