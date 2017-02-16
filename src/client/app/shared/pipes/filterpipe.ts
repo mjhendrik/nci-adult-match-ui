7c3245e5c2855acc4f6627ae91d98839f1d8f90d
Subhash Vallala <sgvallala@gmail.com>
2017-02-16 09:35:17 -0500

    Formatting

--- src/client/app/shared/pipes/filterpipe.ts
+++ src/client/app/shared/pipes/filterpipe.ts
@@ -4,7 +4,7 @@ import { Pipe, PipeTransform, Injectable } from '@angular/core';
 })
 export class filterByText implements PipeTransform {
     transform(items: any[], filterText: string): any[] {
-        var keys=Object.keys(items[0]);
+        var keys = Object.keys(items[0]);
         return items.filter(item => {
             return Object.keys(item).some(key => item[key].toString().toLowerCase().includes(filterText.toLowerCase()));
         }
