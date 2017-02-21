318164f994572d93a862b2ed2563831265391562
Subhash Vallala <sgvallala@gmail.com>
2017-02-21 15:28:14 -0500

    Hide navbar when not logged in

--- src/client/app/shared/navbar/navbar.component.ts
+++ src/client/app/shared/navbar/navbar.component.ts
@@ -1,12 +1,17 @@
+import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
 import { Component } from '@angular/core';
 
 /**
  * This class represents the navigation bar component.
  */
 @Component({
+  providers: [Location, { provide: LocationStrategy, useClass: PathLocationStrategy }],
   moduleId: module.id,
   selector: 'sd-navbar',
   templateUrl: 'navbar.component.html',
   styleUrls: ['navbar.component.css'],
 })
-export class NavbarComponent { }
+export class NavbarComponent {
+  location: Location;
+  constructor(location: Location) { this.location = location; }
+}
