1b2d8630fab212d1fe0ee8962202e48e8c87be90
Subhash Vallala <sgvallala@gmail.com>
2017-02-27 16:15:23 -0500

    Added table to patients tab

--- src/client/app/patients/patients.module.ts
+++ src/client/app/patients/patients.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PatientsComponent } from './patients.component';
 import { PatientsRoutingModule } from './patients-routing.module';
+import { FormsModule } from '@angular/forms';
+import { DataTableModule } from "../shared/datatables/index";
+import { PipesModule } from "../shared/pipes/pipes.module"
 
 @NgModule({
-  imports: [CommonModule, PatientsRoutingModule],
+  imports: [CommonModule, PatientsRoutingModule, FormsModule, DataTableModule, PipesModule],
   declarations: [PatientsComponent],
   exports: [PatientsComponent]
 })
