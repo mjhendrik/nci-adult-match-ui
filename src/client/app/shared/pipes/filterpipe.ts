37eea56c237a13311e450b4f93498833d98fbcdd
Subhash Vallala <sgvallala@gmail.com>
2017-02-28 10:09:40 -0500

    null search filter bug fix

--- src/client/app/shared/pipes/filterpipe.ts
+++ src/client/app/shared/pipes/filterpipe.ts
@@ -6,7 +6,7 @@ export class filterByText implements PipeTransform {
     transform(items: any[], filterText: string): any[] {
         var keys = Object.keys(items[0]);
         return items.filter(item => {
-            return Object.keys(item).some(key => item[key].toString().toLowerCase().includes(filterText.toLowerCase()));
+            return Object.keys(item).some(key =>item[key]!=null ? item[key].toString().toLowerCase().includes(filterText.toLowerCase()):false);
         }
         )
     }
