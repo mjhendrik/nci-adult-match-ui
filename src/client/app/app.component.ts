65a166673a9c6cceb11df967329df869e4fe346a
Subhash Vallala <sgvallala@gmail.com>
2017-03-01 10:59:21 -0500

    Changed text wraping for ta table

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -9,6 +9,7 @@ import './operators';
   moduleId: module.id,
   selector: 'sd-app',
   templateUrl: 'app.component.html',
+  // styleUrls: ['scss/main.css'] // fix_me: main-styles
 })
 export class AppComponent {
 }
