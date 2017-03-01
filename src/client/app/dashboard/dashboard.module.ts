b04e3a2cf07090f62cb96240b3622f2062899417
Rick Zakharov <rick.zakharov@gmail.com>
2017-03-01 14:40:27 -0500

    Removed original seed data service

--- src/client/app/dashboard/dashboard.module.ts
+++ src/client/app/dashboard/dashboard.module.ts
@@ -3,17 +3,15 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { SharedModule } from '../shared/shared.module';
-import { NameListService } from '../shared/name-list/name-list.service';
 import { colorCodeDays } from '../shared/directives/colorcode-days';
 import { colorCodeHours } from '../shared/directives/colorcode-hours';
 import { FormsModule } from '@angular/forms';
-import { DataTableModule } from "../shared/datatables/index";
-import { PipesModule } from "../shared/pipes/pipes.module"
+import { DataTableModule } from '../shared/datatables/index';
+import { PipesModule } from '../shared/pipes/pipes.module';
 
 @NgModule({
   imports: [CommonModule, DashboardRoutingModule, SharedModule, FormsModule, DataTableModule, PipesModule],
   declarations: [DashboardComponent, colorCodeDays, colorCodeHours],
-  exports: [DashboardComponent],
-  providers: [NameListService]
+  exports: [DashboardComponent]
 })
 export class DashboardModule { }
