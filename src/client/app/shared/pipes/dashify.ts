692d7d4bdab316dd3370a2ed77b303f7a1d86ca4
Subhash Vallala <sgvallala@gmail.com>
2017-03-08 10:59:41 -0500

    Added  dashify pipe

---
+++ src/client/app/shared/pipes/dashify.ts
@@ -0,0 +1,9 @@
+import { Pipe, PipeTransform, Injectable } from '@angular/core';
+@Pipe({
+    name: 'dashify'
+})
+export class dashify implements PipeTransform {
+    transform(text: string): string {
+        return text ? text : '-';
+    }
+}
--- src/client/app/shared/pipes/pipes.module.ts
+++ src/client/app/shared/pipes/pipes.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { filterByText } from './filterpipe';
+import { dashify } from './dashify'
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [filterByText],
-  exports: [filterByText],
+  declarations: [filterByText, dashify],
+  exports: [filterByText, dashify],
 })
 export class PipesModule { }
