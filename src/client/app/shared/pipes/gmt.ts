6615ba2978481e43b286aa55f1668c66863fe927
Subhash Vallala <sgvallala@gmail.com>
2017-03-14 17:24:06 -0400

    Fixed table search date

--- src/client/app/shared/pipes/gmt.ts
+++ src/client/app/shared/pipes/gmt.ts
@@ -4,8 +4,15 @@ import * as moment from 'moment/moment';
     name: 'gmt'
 })
 export class GMTFilter implements PipeTransform {
-    transform(date: number): string {
-        if (date !== undefined && date) {
+    transform(inputdate: string): string {
+        let date: number;
+        if (inputdate !== undefined && inputdate !== null) {
+            inputdate = inputdate.toString();
+            if (inputdate.indexOf('-') === -1) {
+                date = parseInt(inputdate);
+            } else {
+                date = new Date(inputdate).getTime();
+            }
             return moment.unix(date / 1000).utc().format('LLL') + ' GMT';
         } else {
             return '-';
