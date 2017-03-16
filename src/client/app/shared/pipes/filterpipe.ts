727280e0358ec42db0326b17015ff3636d7ce694
Subhash Vallala <sgvallala@gmail.com>
2017-03-16 14:00:46 -0400

    Filter pipe bug fix

--- src/client/app/shared/pipes/filterpipe.ts
+++ src/client/app/shared/pipes/filterpipe.ts
@@ -5,8 +5,13 @@ import { Pipe, PipeTransform, Injectable } from '@angular/core';
 export class filterByText implements PipeTransform {
     transform(items: any[], filterText: string): any[] {
         var keys = Object.keys(items[0]);
+        var pattern = keys.join('|');
+        pattern = pattern + '"|{|}';
+        var regex: RegExp = new RegExp(pattern, 'gi');
         return items.filter(item => {
-            return JSON.stringify(item).toLocaleLowerCase().indexOf(filterText.toLocaleLowerCase()) != -1 ? true : false;
+            // console.log(JSON.stringify(item).replace(regex, ''));
+            return JSON.stringify(item).replace(regex, '').toLocaleLowerCase()
+                .indexOf(filterText.toLocaleLowerCase()) !== -1 ? true : false;
         }
         );
     }
