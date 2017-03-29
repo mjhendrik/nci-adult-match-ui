0d023385a1ef75259abf9ec087bb8d92a53230e0
Rick Zakharov <rick.zakharov@gmail.com>
2017-03-29 11:50:53 -0400

    Added patient status colorcode

--- src/client/app/shared/directives/directives.module.ts
+++ src/client/app/shared/directives/directives.module.ts
@@ -8,13 +8,30 @@ import { colorCodeHours } from './colorcode-hours';
 import { colorCodeClia } from './colorcode-clia';
 import { MatchHeightDirective } from './match-height.directive';
 import { ExternalLinksComponent } from './external-links.component';
+import { colorCodeAssignment } from './colorcode-assignment';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [colorCodeTa, colorCodePatients, colorCodeDays, colorCodeHours,
-    colorCodeClia, MatchHeightDirective, ExternalLinksComponent],
-  exports: [colorCodeTa, colorCodePatients, colorCodeDays, colorCodeHours, colorCodeClia,
-    MatchHeightDirective, ExternalLinksComponent],
+  declarations: [
+    colorCodeTa,
+    colorCodePatients,
+    colorCodeDays,
+    colorCodeHours,
+    colorCodeClia,
+    MatchHeightDirective,
+    ExternalLinksComponent,
+    colorCodeAssignment
+  ],
+  exports: [
+    colorCodeTa,
+    colorCodePatients,
+    colorCodeDays,
+    colorCodeHours,
+    colorCodeClia,
+    MatchHeightDirective,
+    ExternalLinksComponent,
+    colorCodeAssignment
+  ],
 })
 export class DirectivesModule {
 }
