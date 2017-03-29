657fb836b88cc05ef87895c9f483aec5dbbea5b7
Rick Zakharov <rick.zakharov@gmail.com>
2017-03-29 12:09:38 -0400

    Added AR and VR status colorcode directives

--- src/client/app/shared/directives/colorcode-variant-report.directive.ts
+++ src/client/app/shared/directives/colorcode-variant-report.directive.ts
@@ -1,18 +1,18 @@
 import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
-@Directive({ selector: '[colorcodeAssignment]' })
-export class ColorCodeAssignmentDirective implements OnInit {
+@Directive({ selector: '[colorcodeVariantReport]' })
+export class ColorCodeVariantReportDirective implements OnInit {
 
-    @Input() 'colorcodeAssignment': string;
+    @Input() 'colorcodeVariantReport': string;
 
     constructor(private el: ElementRef) { }
 
     ngOnInit() {
-        if (this.colorcodeAssignment === 'CONFIRMED') {
+        if (this.colorcodeVariantReport === 'CONFIRMED') {
             this.el.nativeElement.className = 'text-success-light';
-        } else if (this.colorcodeAssignment === 'PENDING_CONFIMATION') {
+        } else if (this.colorcodeVariantReport === 'PENDING') {
             this.el.nativeElement.className = 'text-info-light';
-        } else if (this.colorcodeAssignment === 'REJECTED') {
+        } else if (this.colorcodeVariantReport === 'REJECTED') {
             this.el.nativeElement.className = 'text-danger-light';
         }
     }
