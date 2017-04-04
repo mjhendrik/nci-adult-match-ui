8124a78ec092388533ee1dc64c93d4bf250fafe0
Subhash Vallala <sgvallala@gmail.com>
2017-04-04 11:00:13 -0400

    Unit test config

--- test-config.js
+++ test-config.js
@@ -6,7 +6,7 @@ System.config({
     'angular2/*': 'node_modules/angular2/*.js',
     'rxjs/*': 'node_modules/rxjs/*.js',
     'angular2-jwt/*': 'node_modules/angular2-jwt/*.js',
-    'ng2-charts/*':'node_modules/ng2-charts/*.js',
-    'lodash/*':'node_modules/lodash/*.js',
+    'ng2-charts/*': 'node_modules/ng2-charts/*.js',
+    'lodash/*': 'node_modules/lodash/*.js',
   }
 });
