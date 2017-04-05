7a4b3850c2e4347adf7a72bf176ed6b24eff4304
Subhash Vallala <sgvallala@gmail.com>
2017-04-05 15:17:42 -0400

    CLIA labs changes

--- src/client/app/clia/clia-MDACC/clia.component.ts
+++ src/client/app/clia/clia-MDACC/clia.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
-import { routerTransition } from './../shared/router.animations';
-import { GMTFilter } from './../shared/pipes/gmt';
+import { routerTransition } from './../../shared/router.animations';
+import { GMTFilter } from './../../shared/pipes/gmt';
 
 
 /**
