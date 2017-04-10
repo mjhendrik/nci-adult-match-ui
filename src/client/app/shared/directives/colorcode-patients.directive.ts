3866ad9268ec1d4e49bcd4ee5920af494e483887
Subhash Vallala <sgvallala@gmail.com>
2017-04-10 17:18:15 -0400

    Color code patients bug fix

--- src/client/app/shared/directives/colorcode-patients.directive.ts
+++ src/client/app/shared/directives/colorcode-patients.directive.ts
@@ -13,11 +13,11 @@ export class ColorCodePatientsDirective implements OnInit {
         } else if (this.colorCodePatients === 'REGISTRATION') {
             this.el.nativeElement.classList.add('text-info-light');
         } else if (this.colorCodePatients === 'COMPASSIONATE_CARE' || this.colorCodePatients === 'PENDING_APPROVAL'
-            || this.colorCodePatients === 'PENDING_colorCodePatientsFIRMATION' || this.colorCodePatients === 'REJOIN_REQUESTED'
+            || this.colorCodePatients === 'PENDING_CONFIRMATION' || this.colorCodePatients === 'REJOIN_REQUESTED'
             || this.colorCodePatients === 'NOT_ELIGIBLE') {
             this.el.nativeElement.classList.add('text-purple-light');
         } else if (this.colorCodePatients === 'OFF_TRIAL_DECEASED' || this.colorCodePatients === 'OFF_TRIAL_NO_TA_AVAILABLE'
-            || this.colorCodePatients === 'OFF_TRIAL_NOT_colorCodePatientsSENTED') {
+            || this.colorCodePatients === 'OFF_TRIAL_NOT_CONSENTED') {
             this.el.nativeElement.classList.add('text-danger-light');
         } else if (this.colorCodePatients === 'FORMERLY_ON_ARM_OFF_TRIAL') {
             this.el.nativeElement.classList.add('text-warning-light');
