24a46653de2141d1bdb773b219680b4ff6dfc4a4
Rick Zakharov <rick.zakharov@gmail.com>
2017-04-10 13:50:11 -0400

    Added PatientApiService that pulls data from a mock json

--- src/client/app/patient/patient-list/patient-list.module.ts
+++ src/client/app/patient/patient-list/patient-list.module.ts
@@ -6,10 +6,12 @@ import { FormsModule } from '@angular/forms';
 import { DataTableModule } from '../../shared/datatables/index';
 import { PipesModule } from '../../shared/pipes/pipes.module';
 import { DirectivesModule } from '../../shared/directives/directives.module';
+import { PatientApiService } from '../patient-api.service';
 
 @NgModule({
   imports: [CommonModule, PatientListRoutingModule, FormsModule, DataTableModule, PipesModule, DirectivesModule],
   declarations: [PatientListComponent],
-  exports: [PatientListComponent]
+  exports: [PatientListComponent],
+  providers: [PatientApiService]
 })
 export class PatientListModule { }
