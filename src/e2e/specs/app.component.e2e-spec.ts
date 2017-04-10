5f8795fc66b850e7e414ff72e0ffaf0ccf8a9e48
Rick Zakharov <rick.zakharov@gmail.com>
2017-04-10 18:02:19 -0400

    Upgrading to angular 4

--- src/e2e/specs/app.component.e2e-spec.ts
+++ src/e2e/specs/app.component.e2e-spec.ts
@@ -3,35 +3,23 @@ import { browser, element, by } from 'protractor';
 describe('App', () => {
 
   beforeEach(async () => {
-    return await browser.get('');
+    return await browser.get('/');
   });
 
   it('should have a title', () => {
-    expect(browser.getTitle()).toEqual('Welcome to nci-adult-match-ui!');
+    expect(browser.getTitle()).toEqual('Welcome to angular-seed!');
   });
 
   it('should have <nav>', () => {
     expect(element(by.css('sd-app sd-navbar nav')).isPresent()).toEqual(true);
   });
 
-  it('should have correct nav text for dashboard', () => {
-    expect(element(by.css('sd-app sd-navbar nav a:first-child')).getText()).toEqual('dashboard');
+  it('should have correct nav text for Home', () => {
+    expect(element(by.css('sd-app sd-navbar nav a:first-child')).getText()).toEqual('HOME');
   });
 
-  it('should have correct nav text for patients', () => {
-    expect(element(by.css('sd-app sd-navbar nav a:nth-child(2)')).getText()).toEqual('patients');
-  });
-
-  it('should have correct nav text for treatment arms', () => {
-    expect(element(by.css('sd-app sd-navbar nav a:nth-child(3)')).getText()).toEqual('treatment arms');
-  });
-
-  it('should have correct nav text for biopsy tracking', () => {
-    expect(element(by.css('sd-app sd-navbar nav a:nth-child(4)')).getText()).toEqual('biopsy tracking');
-  });
-
-  it('should have correct nav text for clia labs', () => {
-    expect(element(by.css('sd-app sd-navbar nav a:nth-child(5)')).getText()).toEqual('clia labs');
+  it('should have correct nav text for About', () => {
+    expect(element(by.css('sd-app sd-navbar nav a:nth-child(2)')).getText()).toEqual('ABOUT');
   });
 
 });
