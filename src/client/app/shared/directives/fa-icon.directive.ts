32e55325b664798226a6463658e737b9d5dfd500
Subhash Vallala <sgvallala@gmail.com>
2017-04-12 10:59:38 -0400

    Added fa-icon directive

---
+++ src/client/app/shared/directives/fa-icon.directive.ts
@@ -0,0 +1,22 @@
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+
+@Directive({ selector: '[faIcon]' })
+export class FaIconDirective implements OnInit {
+
+    @Input() 'faIcon': string;
+
+    constructor(private el: ElementRef) { }
+
+    ngOnInit() {
+        console.log(this.faIcon);
+        if (this.faIcon === 'bar-chart') {
+            this.el.nativeElement.classList.add('fa-bar-chart');
+        } else if (this.faIcon === 'user') {
+            this.el.nativeElement.classList.add('fa-user');
+        } else if (this.faIcon === 'medkit') {
+            this.el.nativeElement.classList.add('fa-medkit');
+        }
+
+    }
+
+}
