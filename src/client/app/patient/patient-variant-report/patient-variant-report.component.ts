ee6f804062477e66e587a269db6d8d9ccead6c59
Subhash Vallala <sgvallala@gmail.com>
2017-04-12 11:35:04 -0400

    Adding patient-variant-report json

--- src/client/app/patient/patient-variant-report/patient-variant-report.component.ts
+++ src/client/app/patient/patient-variant-report/patient-variant-report.component.ts
@@ -1,6 +1,4 @@
-import {
-  Component
-} from '@angular/core';
+import { Component } from '@angular/core';
 
 import { routerTransition } from './../../shared/router.animations';
 
@@ -16,7 +14,25 @@ import { routerTransition } from './../../shared/router.animations';
   host: { '[@routerTransition]': '' }
 })
 export class PatientVariantReportComponent {
+
   scrollToAssignmentDetails(): void {
     setTimeout(() => window.scrollTo('assignment-details'), 1);
   }
+
+  variantReport: any = {
+    "patientSequenceNumber": "11276",
+    "patientStatus": "PENDING_CONFIRMATION",
+    "step": 0,
+    "concordance": "YES",
+    "variantReportStatus": "CONFIRMED",
+    "variantReportDate": "August 26, 2016 3:28 PM GMT",
+    "biopsySequenceNumber": "T-16-000762",
+    "molecularSequenceNumber": "MSN17772",
+    "analysisId": "MSN17772_v1_92ad9833-e79a-4807-b1f3-6be88a0ab824",
+    "mapd": "0.317",
+    "cellularity": "1.000000",
+    "fileReceivedDate": "August 25, 2016 10:13 PM GMT",
+    "torrentVariantCallerVersion": "5.0-9"
+  };
+
 }
