bed4900be8e7e637ecc631263b36e0157da781c1
Rick Zakharov <rick.zakharov@gmail.com>
2017-04-13 10:56:06 -0400

    Configuring coverage report

--- src/client/app/app.component.spec.ts
+++ src/client/app/app.component.spec.ts
@@ -8,69 +8,69 @@ import { Route } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
-// import { LoginComponent } from './login/login.component';
-// import { DashboardComponent } from './dashboard/dashboard.component';
-// import { PatientListComponent } from './patient/patient-list/patient-list.component';
-// import { TreatmentArmListComponent } from './treatment-arm/treatment-arm-list/treatment-arm-list.component';
-// import { PatientDetailsComponent } from './patient/patient-details/patient-details.component';
-// import { PatientVariantReportComponent } from './patient/patient-variant-report/patient-variant-report.component';
-// import { TreatmentArmDetailsComponent } from './treatment-arm/treatment-arm-details/treatment-arm-details.component';
-// import { BiopsyTrackingListComponent } from './biopsy-tracking/biopsy-tracking.component';
-// import { CliaDartmouthComponent } from './clia/clia-dartmouth/clia-dartmouth.component';
-// import { CliaMdaccComponent } from './clia/clia-mdacc/clia-mdacc.component';
-// import { CliaMghComponent } from './clia/clia-mgh/clia-mgh.component';
-// import { CliaMochaComponent } from './clia/clia-mocha/clia-mocha.component';
-// import { CliaYaleComponent } from './clia/clia-yale/clia-yale.component';
-// import { CliaVariantReportsNtcComponent } from './clia/clia-variant-reports-ntc/clia-variant-reports-ntc.component';
-// import { CliaVariantReportsPaccComponent } from './clia/clia-variant-reports-pacc/clia-variant-reports-pacc.component';
-// import { CliaVariantReportsPcComponent } from './clia/clia-variant-reports-pc/clia-variant-reports-pc.component';
-// import { NavbarComponent } from './shared/navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PatientListComponent } from './patient/patient-list/patient-list.component';
+import { TreatmentArmListComponent } from './treatment-arm/treatment-arm-list/treatment-arm-list.component';
+import { PatientDetailsComponent } from './patient/patient-details/patient-details.component';
+import { PatientVariantReportComponent } from './patient/patient-variant-report/patient-variant-report.component';
+import { TreatmentArmDetailsComponent } from './treatment-arm/treatment-arm-details/treatment-arm-details.component';
+import { BiopsyTrackingListComponent } from './biopsy-tracking/biopsy-tracking.component';
+import { CliaDartmouthComponent } from './clia/clia-dartmouth/clia-dartmouth.component';
+import { CliaMdaccComponent } from './clia/clia-mdacc/clia-mdacc.component';
+import { CliaMghComponent } from './clia/clia-mgh/clia-mgh.component';
+import { CliaMochaComponent } from './clia/clia-mocha/clia-mocha.component';
+import { CliaYaleComponent } from './clia/clia-yale/clia-yale.component';
+import { CliaVariantReportsNtcComponent } from './clia/clia-variant-reports-ntc/clia-variant-reports-ntc.component';
+import { CliaVariantReportsPaccComponent } from './clia/clia-variant-reports-pacc/clia-variant-reports-pacc.component';
+import { CliaVariantReportsPcComponent } from './clia/clia-variant-reports-pc/clia-variant-reports-pc.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 export function main() {
 
   describe('App component', () => {
 
     let config: Route[] = [
-      // { path: '', component: LoginComponent },
-      // { path: 'dashboard', component: DashboardComponent },
-      // { path: 'patients', component: PatientListComponent },
-      // { path: 'patients/details', component: PatientDetailsComponent },
-      // { path: 'patients/variant_report', component: PatientVariantReportComponent },
-      // { path: 'treatments', component: TreatmentArmListComponent },
-      // { path: 'treatments/details', component: TreatmentArmDetailsComponent },
-      // { path: 'tracking', component: BiopsyTrackingListComponent },
-      // { path: 'clia_dartmouth', component: CliaDartmouthComponent },
-      // { path: 'clia_mdacc', component: CliaMdaccComponent },
-      // { path: 'clia_mgh', component: CliaMghComponent },
-      // { path: 'clia_mocha', component: CliaMochaComponent },
-      // { path: 'clia_yale', component: CliaYaleComponent },
-      // { path: 'clia_variant_reports_ntc', component: CliaVariantReportsNtcComponent },
-      // { path: 'clia_variant_reports_pacc', component: CliaVariantReportsPaccComponent },
-      // { path: 'clia_variant_reports_pc', component: CliaVariantReportsPcComponent }
+      { path: '', component: LoginComponent },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'patients', component: PatientListComponent },
+      { path: 'patients/details', component: PatientDetailsComponent },
+      { path: 'patients/variant_report', component: PatientVariantReportComponent },
+      { path: 'treatments', component: TreatmentArmListComponent },
+      { path: 'treatments/details', component: TreatmentArmDetailsComponent },
+      { path: 'tracking', component: BiopsyTrackingListComponent },
+      { path: 'clia_dartmouth', component: CliaDartmouthComponent },
+      { path: 'clia_mdacc', component: CliaMdaccComponent },
+      { path: 'clia_mgh', component: CliaMghComponent },
+      { path: 'clia_mocha', component: CliaMochaComponent },
+      { path: 'clia_yale', component: CliaYaleComponent },
+      { path: 'clia_variant_reports_ntc', component: CliaVariantReportsNtcComponent },
+      { path: 'clia_variant_reports_pacc', component: CliaVariantReportsPaccComponent },
+      { path: 'clia_variant_reports_pc', component: CliaVariantReportsPcComponent }
     ];
     beforeEach(() => {
       TestBed.configureTestingModule({
         imports: [FormsModule, RouterTestingModule.withRoutes(config)],
         declarations: [
           TestComponent,
-          AppComponent
-          // NavbarComponent,
-          // LoginComponent,
-          // DashboardComponent,
-          // PatientListComponent,
-          // TreatmentArmListComponent,
-          // TreatmentArmDetailsComponent,
-          // BiopsyTrackingListComponent,
-          // CliaDartmouthComponent,
-          // CliaMdaccComponent,
-          // CliaMghComponent,
-          // CliaMochaComponent,
-          // CliaYaleComponent,
-          // CliaVariantReportsNtcComponent,
-          // CliaVariantReportsPaccComponent,
-          // CliaVariantReportsPcComponent,
-          // PatientDetailsComponent,
-          // PatientVariantReportComponent,
+          AppComponent,
+          NavbarComponent,
+          LoginComponent,
+          DashboardComponent,
+          PatientListComponent,
+          TreatmentArmListComponent,
+          TreatmentArmDetailsComponent,
+          BiopsyTrackingListComponent,
+          CliaDartmouthComponent,
+          CliaMdaccComponent,
+          CliaMghComponent,
+          CliaMochaComponent,
+          CliaYaleComponent,
+          CliaVariantReportsNtcComponent,
+          CliaVariantReportsPaccComponent,
+          CliaVariantReportsPcComponent,
+          PatientDetailsComponent,
+          PatientVariantReportComponent,
         ],
         providers: [
           { provide: APP_BASE_HREF, useValue: '' }
