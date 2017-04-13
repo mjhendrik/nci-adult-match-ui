440679d10fe1bc88e42bc0fbfd9d6dab03e5a56a
Subhash Vallala <sgvallala@gmail.com>
2017-04-13 17:12:43 -0400

    Adding patient-api service

--- src/client/app/patient/patient-variant-report/patient-variant-report.module.ts
+++ src/client/app/patient/patient-variant-report/patient-variant-report.module.ts
@@ -6,10 +6,12 @@ import { FormsModule } from '@angular/forms';
 import { DataTableModule } from '../../shared/datatables/index';
 import { PipesModule } from '../../shared/pipes/pipes.module';
 import { DirectivesModule } from '../../shared/directives/directives.module';
+import { PatientApiService } from '../patient-api.service';
 
 @NgModule({
   imports: [CommonModule, PatientVariantReportRoutingModule, FormsModule, DataTableModule, PipesModule, DirectivesModule],
   declarations: [PatientVariantReportComponent],
-  exports: [PatientVariantReportComponent]
+  exports: [PatientVariantReportComponent],
+  providers: [PatientApiService]
 })
 export class PatientVariantReportModule { }
