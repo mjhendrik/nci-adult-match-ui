b01f07e8cfa598c897ccef1502af07eff403924c
Rick Zakharov <rick.zakharov@gmail.com>
2017-04-14 15:01:28 -0400

    Configuring inttest build

--- tools/env/base.ts
+++ tools/env/base.ts
@@ -2,8 +2,9 @@ import { EnvConfig } from './env-config.interface';
 
 const BaseConfig: EnvConfig = {
   // Sample API url
-  API: 'https://demo.com'
+  API: 'https://demo.com',
+  CLIENT_ID: 'RjoYZXUDEzQxMJw04C6B5dsQKqUAEYzA', // Dev by default
+  AUTH_DOMAIN: 'ncimatch.auth0.com'
 };
 
 export = BaseConfig;
-
--- tools/env/dev.ts
+++ tools/env/dev.ts
@@ -1,7 +1,9 @@
 import { EnvConfig } from './env-config.interface';
 
 const DevConfig: EnvConfig = {
-  ENV: 'DEV'
+  ENV: 'DEV',
+  CLIENT_ID: 'RjoYZXUDEzQxMJw04C6B5dsQKqUAEYzA',
+  AUTH_DOMAIN: 'ncimatch.auth0.com'
 };
 
 export = DevConfig;
