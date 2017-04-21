7507083f8791ddc7597319698cf8a64ed0d3a52d
Rick Zakharov <rick.zakharov@gmail.com>
2017-04-21 11:56:48 -0400

    Implementing Assignment Reason table with TA drugs/diseases

--- src/client/app/patient/assignment-reason-table/assignment-reason-table.module.ts
+++ src/client/app/patient/assignment-reason-table/assignment-reason-table.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { AssignmentReasonTableComponent } from './assignment-reason-table.component';
 import { FormsModule } from '@angular/forms';
 import { DataTableModule } from '../../shared/datatables/index';
@@ -8,6 +9,7 @@ import { DirectivesModule } from '../../shared/directives/directives.module';
 
 @NgModule({
   imports: [
+    RouterModule,
     CommonModule,
     FormsModule,
     DataTableModule,
