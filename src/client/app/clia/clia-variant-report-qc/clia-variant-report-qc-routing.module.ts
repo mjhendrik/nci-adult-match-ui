160d099fe0a1ce55d59cab0f71b163deca18a10f
Subhash Vallala <sgvallala@gmail.com>
2017-04-24 13:50:26 -0400

    Make URLs consistent throughout app

--- src/client/app/clia/clia-variant-report-qc/clia-variant-report-qc-routing.module.ts
+++ src/client/app/clia/clia-variant-report-qc/clia-variant-report-qc-routing.module.ts
@@ -7,7 +7,21 @@ import { AuthGuard } from './../../shared/auth/auth.guard.service';
 @NgModule({
   imports: [
     RouterModule.forChild([
-      { path: 'clia_variant_reports_qc/:tabType/:cliaType', component: CliaVariantReportQcComponent, canActivate: [AuthGuard] }
+      { path: 'clia_mocha/variant_reports_pc/qc/:tabType/:cliaType', component: CliaVariantReportQcComponent, canActivate: [AuthGuard] },
+      { path: 'clia_mgh/variant_reports_pc/qc/:tabType/:cliaType', component: CliaVariantReportQcComponent, canActivate: [AuthGuard] },
+      { path: 'clia_dartmount/variant_reports_pc/qc/:tabType/:cliaType', component: CliaVariantReportQcComponent, canActivate: [AuthGuard] },
+      { path: 'clia_yale/variant_reports_pc/qc/:tabType/:cliaType', component: CliaVariantReportQcComponent, canActivate: [AuthGuard] },
+      { path: 'clia_mdacc/variant_reports_pc/qc/:tabType/:cliaType', component: CliaVariantReportQcComponent, canActivate: [AuthGuard] },
+      { path: 'clia_mocha/variant_reports_ntc/qc/:tabType/:cliaType', component: CliaVariantReportQcComponent, canActivate: [AuthGuard] },
+      { path: 'clia_mgh/variant_reports_ntc/qc/:tabType/:cliaType', component: CliaVariantReportQcComponent, canActivate: [AuthGuard] },
+      { path: 'clia_dartmount/variant_reports_ntc/qc/:tabType/:cliaType', component: CliaVariantReportQcComponent, canActivate: [AuthGuard] },
+      { path: 'clia_yale/variant_reports_ntc/qc/:tabType/:cliaType', component: CliaVariantReportQcComponent, canActivate: [AuthGuard] },
+      { path: 'clia_mdacc/variant_reports_ntc/qc/:tabType/:cliaType', component: CliaVariantReportQcComponent, canActivate: [AuthGuard] },
+      { path: 'clia_mocha/variant_reports_pacc/qc/:tabType/:cliaType', component: CliaVariantReportQcComponent, canActivate: [AuthGuard] },
+      { path: 'clia_mgh/variant_reports_pacc/qc/:tabType/:cliaType', component: CliaVariantReportQcComponent, canActivate: [AuthGuard] },
+      { path: 'clia_dartmount/variant_reports_pacc/qc/:tabType/:cliaType', component: CliaVariantReportQcComponent, canActivate: [AuthGuard] },
+      { path: 'clia_yale/variant_reports_pacc/qc/:tabType/:cliaType', component: CliaVariantReportQcComponent, canActivate: [AuthGuard] },
+      { path: 'clia_mdacc/variant_reports_pacc/qc/:tabType/:cliaType', component: CliaVariantReportQcComponent, canActivate: [AuthGuard] }
     ])
   ],
   exports: [RouterModule]
