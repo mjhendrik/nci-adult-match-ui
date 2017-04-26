b7f2206a971d692720299693de83fc5ddf32fef2
Subhash Vallala <sgvallala@gmail.com>
2017-04-26 12:31:22 -0400

    Dropzone file upload changes

--- src/client/app/patient/patient-details/patient-details.component.ts
+++ src/client/app/patient/patient-details/patient-details.component.ts
@@ -2,6 +2,7 @@ import {
   Component,
   OnInit
 } from '@angular/core';
+import { DropzoneConfigInterface } from 'ngx-dropzone-wrapper';
 
 import { routerTransition } from './../../shared/router.animations';
 import {
@@ -28,10 +29,37 @@ export class PatientDetailsComponent implements OnInit {
   dataAvailable: boolean;
   errorMessage: string;
 
+  analysisId: string = '';
+
+  config1: DropzoneConfigInterface;
+  config2: DropzoneConfigInterface;
+
   constructor(private patientApi: PatientApiService) { }
 
   ngOnInit() {
     this.getData();
+
+    const DROPZONE_CONFIG1: DropzoneConfigInterface = {
+      // Change this to your upload POST address:
+      server: 'https://httpbin.org/post',
+      maxFiles: 3,
+      maxFilesize: 20000, // size in MB
+      addRemoveLinks: true,
+      acceptedFiles: '.zip,.bam'
+    };
+
+    this.config1 = DROPZONE_CONFIG1;
+
+    const DROPZONE_CONFIG2: DropzoneConfigInterface = {
+      // Change this to your upload POST address:
+      server: 'https://httpbin.org/post',
+      maxFilesize: 20000, // size in MB
+      addRemoveLinks: true
+      // acceptedFiles: '.zip,.bam'
+    };
+
+    this.config2 = DROPZONE_CONFIG2;
+
   }
 
   getData() {
@@ -45,5 +73,14 @@ export class PatientDetailsComponent implements OnInit {
       error => this.errorMessage = <any>error
       );
   }
+
+  onUploadSuccess(evt: any): void {
+    console.log(evt);
+  }
+
+  onUploadError(evt: any): void {
+    console.log(evt);
+  }
+
 
 }
