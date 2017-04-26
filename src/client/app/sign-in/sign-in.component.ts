41f9a7fadcb7af064a05b58d194bedf99f0225f6
Rick Zakharov <rick.zakharov@gmail.com>
2017-04-26 15:30:08 -0400

    Implementing custom login page

--- src/client/app/sign-in/sign-in.component.ts
+++ src/client/app/sign-in/sign-in.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+
+import { routerTransition } from './../shared/router.animations';
 import { Auth } from './../shared/auth/auth.service';
 
+/**
+ * SignInComponent.
+ */
 @Component({
-  selector: 'sign-in',
-  templateUrl: 'sign-in.component.html'
+  moduleId: module.id,
+  selector: 'sd-sign-in',
+  templateUrl: 'sign-in.component.html',
+  animations: [routerTransition()],
+  host: { '[@routerTransition]': '' }
 })
 export class SignInComponent {
-  constructor(private auth: Auth) {}
-};
+  constructor(private auth: Auth) { }
+}
