244c498def0eadc363625d85f3e20ceccadbc802
Rick Zakharov <rick.zakharov@gmail.com>
2017-04-26 15:30:08 -0400

    Implementing custom login page

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import './operators';
 
+import { Auth } from './shared/auth/auth.service';
+
 /**
  * This class represents the main application component.
  */
@@ -12,12 +14,13 @@ import './operators';
 })
 export class AppComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private auth: Auth, private router: Router) {
+    this.auth.handleAuthentication();
+  }
 
   ngOnInit() {
     this.router.events.filter(event => event instanceof NavigationEnd).subscribe(event => {
       window.scroll(0, 0);
     });
   }
-
 }
