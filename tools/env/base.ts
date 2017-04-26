cea33d129da1c929c132521e7bf3360891740abe
Rick Zakharov <rick.zakharov@gmail.com>
2017-04-26 16:24:14 -0400

    Implementing custom login page

--- tools/env/base.ts
+++ tools/env/base.ts
@@ -4,7 +4,7 @@ const BaseConfig: EnvConfig = {
   // Sample API url
   API: 'https://demo.com',
   CLIENT_ID: 'RjoYZXUDEzQxMJw04C6B5dsQKqUAEYzA', // Dev by default,
-  REDIRECT_URL: 'http://localhost:5555/',
+  REDIRECT_URL: 'http://localhost:5555/dashboard',
   AUTH_DOMAIN: 'ncimatch.auth0.com'
 };
 
