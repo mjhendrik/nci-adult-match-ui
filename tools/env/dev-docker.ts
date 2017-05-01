9af0f5449f17bfd5f4560802eaf35f72a8d0586f
Rick Zakharov <rick.zakharov@gmail.com>
2017-05-01 18:14:54 -0400

    Retrieving patient list from api+mongo

--- tools/env/dev-docker.ts
+++ tools/env/dev-docker.ts
@@ -3,7 +3,7 @@ import { EnvConfig } from './env-config.interface';
 const DevConfig: EnvConfig = {
   // Empty URL will force the cervices to use mock jsons instead of calling the real backend services
   API: {
-    PATIENT: 'http://patient-api/5000',
+    PATIENT: 'http://localhost:5000/api/v1',
     TREATMENT_ARM: '',
     ION_REPORTER: 'http://localhost:5555/api/v1/ion_reporters/healthcheck?site='
   },
