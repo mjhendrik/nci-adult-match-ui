a5111046072831dd09a8bc6e9753b0e4ea863fcf
Subhash Vallala <sgvallala@gmail.com>
2017-05-02 10:09:25 -0400

    Added env config to CLIA Lab IR Ecosystem API

--- tools/env/base.ts
+++ tools/env/base.ts
@@ -3,7 +3,7 @@ import { EnvConfig } from './env-config.interface';
 const BaseConfig: EnvConfig = {
   API: {
     PATIENT: 'http://localhost/5000',
-    ION_REPORTER: 'http://localhost:5555/api/v1/ion_reporters/healthcheck?site=',
+    ION_REPORTER: 'http://localhost:5555/api/v1',
     TREATMENT_ARM: '[TBD]'
   },
   CLIENT_ID: 'RjoYZXUDEzQxMJw04C6B5dsQKqUAEYzA', // Dev by default
--- tools/env/dev.ts
+++ tools/env/dev.ts
@@ -4,7 +4,7 @@ const DevConfig: EnvConfig = {
   // Empty URL will force the cervices to use mock jsons instead of calling the real backend services
   API: {
     PATIENT: 'http://localhost:5000/api/v1',
-    ION_REPORTER: 'http://localhost:5555/api/v1/ion_reporters/healthcheck?site=',
+    ION_REPORTER: 'http://localhost:5555/api/v1',
     TREATMENT_ARM: ''
   },
   CLIENT_ID: 'RjoYZXUDEzQxMJw04C6B5dsQKqUAEYzA',
