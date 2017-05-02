effa3020b6b6d7ece2452df2ac705112f60a656e
Rick Zakharov <rick.zakharov@gmail.com>
2017-05-02 17:35:14 -0400

    Fixed patient router links

--- src/client/app/patient/patient-details/patient-details-routing.module.ts
+++ src/client/app/patient/patient-details/patient-details-routing.module.ts
@@ -7,7 +7,7 @@ import { AuthGuard } from './../../shared/auth/auth.guard.service';
 @NgModule({
   imports: [
     RouterModule.forChild([
-      { path: 'patients/details/:patientSequenceNumber', component: PatientDetailsComponent, canActivate: [AuthGuard] }
+      { path: 'patients/:patientSequenceNumber', component: PatientDetailsComponent, canActivate: [AuthGuard] }
     ])
   ],
   exports: [RouterModule]
