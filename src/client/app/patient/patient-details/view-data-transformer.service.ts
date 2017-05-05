db65ab896774d13c67e54235909c2a836e815397
Rick Zakharov <rick.zakharov@gmail.com>
2017-05-05 14:00:21 -0400

    Implementing Patient Details view

--- src/client/app/patient/patient-details/view-data-transformer.service.ts
+++ src/client/app/patient/patient-details/view-data-transformer.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ViewDataTransformer {
   transformPatient(source: any): any {
-    const transformed: any = {... source}; // Deep-copy the source
+    const transformed: any = { ...source }; // Deep-copy the source
 
     transformed.disease = source.diseases && source.diseases.length ? source.diseases[0] : null;
 
@@ -23,6 +23,38 @@ export class ViewDataTransformer {
 
   transformBiopsy(source: any): any {
     let transformed = source;
+
+    if ('mdAndersonMessages' in source) {
+      transformed.nucleicAcidSendouts = source.nucleicAcidSendouts || [];
+      for (let message of source.mdAndersonMessages) {
+        switch (message.message) {
+          case 'NUCLEIC_ACID_SENDOUT':
+            {
+              let sendout: any = {};
+              transformed.nucleicAcidSendouts.push(sendout);
+
+              sendout.molecularSequenceNumber = message.molecularSequenceNumber;
+              sendout.trackingNumber = message.trackingNumber;
+              sendout.destinationSite = message.destinationSite;
+              sendout.dnaConcentration = message.dnaConcentration;
+              sendout.dnaVolume = message.dnaVolume;
+              sendout.reportedDate = message.reportedDate;
+              sendout.comment = message.comment;
+            }
+            break;
+
+          case 'PATHOLOGY_CONFIRMATION':
+          case 'PATHOLOGY_FAILURE':
+            break;
+
+          case 'SPECIMEN_RECEIVED':
+            break;
+
+          case 'SPECIMEN_FAILURE':
+            break;
+        }
+      }
+    }
 
     return transformed;
   }
