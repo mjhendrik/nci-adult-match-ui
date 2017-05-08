431130d2a780ebf2971122cf92cacd3417eaaa4a
Subhash Vallala <sgvallala@gmail.com>
2017-05-08 09:59:04 -0400

    Unit test setup

--- src/client/app/biopsy-tracking/biopsy-tracking.component.spec.ts
+++ src/client/app/biopsy-tracking/biopsy-tracking.component.spec.ts
@@ -1,38 +1,65 @@
 import { Component } from '@angular/core';
 import {
   async,
-  TestBed
+  TestBed,
+  ComponentFixture
 } from '@angular/core/testing';
 
 import { BiopsyTrackingListModule } from './biopsy-tracking.module';
+import { BiopsyTrackingListComponent } from './biopsy-tracking.component';
+import { BiopsyTrackingApiService } from './biopsy-tracking-api.service';
 
 export function main() {
   describe('biopsy tracking component', () => {
+
+    let component: BiopsyTrackingListComponent;
+    let fixture: ComponentFixture<BiopsyTrackingListComponent>;
     // Setting module for testing
     // Disable old forms
 
     beforeEach(() => {
       TestBed.configureTestingModule({
-        declarations: [TestComponent],
-        imports: [BiopsyTrackingListModule]
-      });
+        declarations: [],
+        imports: [BiopsyTrackingListModule],
+        providers: [
+          {
+            provide: BiopsyTrackingApiService,
+            useClass: MockBiopsyService
+          }
+        ]
+      }).compileComponents();
     });
 
-    it('should work',
-      async(() => {
-        TestBed
-          .compileComponents()
-          .then(() => {
-            let fixture = TestBed.createComponent(TestComponent);
-            let aboutDOMEl = fixture.debugElement.children[0].nativeElement;
+    beforeEach(() => {
+      fixture = TestBed.createComponent(BiopsyTrackingListComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
 
-          });
-      }));
+    // it('should work',
+    //   async(() => {
+    //     // TestBed
+    //     //   .compileComponents()
+    //     //   .then(() => {
+    //     //     let fixture = TestBed.createComponent(TestComponent);
+    //     //     let aboutDOMEl = fixture.debugElement.children[0].nativeElement;
+
+    //     //   });
+    //   }));
+
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
   });
 }
 
-@Component({
-  selector: 'test-cmp',
-  template: '<sd-biopsy-tracking></sd-biopsy-tracking>'
-})
-class TestComponent { }
+class MockBiopsyService {
+  public mockName: string = 'Mocked Service';
+}
+
+// @Component({
+//   selector: 'test-cmp',
+//   template: '<sd-biopsy-tracking></sd-biopsy-tracking>'
+// })
+// class TestComponent { }
