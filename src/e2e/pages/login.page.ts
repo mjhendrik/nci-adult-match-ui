e4ca1e11eade9785c59c5df4b538526e7f17bddb
Rick Zakharov <rick.zakharov@gmail.com>
2017-05-09 17:08:53 -0400

    Implementing Login Page initial test

--- src/e2e/pages/login.page.ts
+++ src/e2e/pages/login.page.ts
@@ -1,6 +1,10 @@
-import { $, by, element, ElementFinder } from 'protractor';
+import { browser, $, by, element, ElementFinder } from 'protractor';
 
 export class LoginPageObject {
   continueButton: ElementFinder = element(by.text('Acknowledge and Continue'));
   loginPopup: ElementFinder = element(by.css('auth0-lock-container'));
+  
+  navigate() : any {
+    browser.get('');
+  }  
 }
