6778b888eb7a67c23dca3ba832e58baabbf49b04
Rick Zakharov <rick.zakharov@gmail.com>
2017-05-09 16:11:26 -0400

    Implementing Login Page initial test

--- src/e2e/step-definitions/login.step-definitions.ts
+++ src/e2e/step-definitions/login.step-definitions.ts
@@ -1,26 +1,28 @@
 import { browser } from 'protractor';
 import { LoginPageObject } from '../pages/login.page';
-import { defineSupportCode } from 'cucumber';
 let chai = require('chai').use(require('chai-as-promised'));
 let expect = chai.expect;
 
-/*
-StepDefinition files act as the glue code between config and feature files
-They drive the feature files from the background
-**/
-defineSupportCode(({Given, When, Then}) => {
-  let page: LoginPageObject = new LoginPageObject();
+import { binding, given, when, then } from 'cucumber-tsflow';
+import { CallbackStepDefinition } from 'cucumber';
 
-  Given(/^I am on login page with title "(.*?)"$/, (url: string) => {
-    return browser.get(url);
-  });
+@binding()
+class LoginSteps {
+  private page = new LoginPageObject();
+  
+  @given(/^I am on login page with title '(.*)'$/)
+  private given(title: string, callback: CallbackStepDefinition) {
+    expect(browser.getTitle()).to.eventually.be.equal(title).and.notify(callback);
+  }
+ 
+  @when(/^I click Acknowledge and Continue button$/)
+  private when(callback: CallbackStepDefinition) {
+    this.page.continueButton.click();
+    callback();
+  }
 
-  When(/^I click Acknowledge and Continue button$/, () => {
-    return page.continueButton.click();
-  });
-
-  Then(/^I should see login popup$/, (result: string) => {
-    return expect(page.loginPopup).to.eventually.be.present();
-  });
-})
-
+  @then(/^I should see login popup$/)
+  private then(callback: CallbackStepDefinition) {
+    expect(this.page.loginPopup).to.eventually.be.present().and.notify(callback);
+  }
+}
