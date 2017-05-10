2de8efd004b739a0bae0f163ba0a23463840fbd0
Subhash Vallala <sgvallala@gmail.com>
2017-05-10 17:41:39 -0400

    Unit tests

--- src/client/app/clia/clia-variant-reports-ntc/clia-variant-reports-ntc.component.spec.ts
+++ src/client/app/clia/clia-variant-reports-ntc/clia-variant-reports-ntc.component.spec.ts
@@ -1,39 +1,74 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
 import {
   async,
   TestBed
 } from '@angular/core/testing';
+import { Observable } from 'rxjs/Observable';
 
-import { CliaVariantReportsNtcModule } from './clia-variant-reports-ntc.module';
+// import { CliaVariantReportsNtcModule } from './clia-variant-reports-ntc.module';
+import { CliaVariantReportsNtcComponent } from './clia-variant-reports-ntc.component';
+// import { CliaVariantReportsNtcRoutingModule } from './clia-variant-reports-ntc-routing.module';
+import { CliaApiService, CliaVariantReportsNTCInterface } from './../clia-api.service';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { PipesModule } from './../../shared/pipes/pipes.module';
+import { DirectivesModule } from './../../shared/directives/directives.module';
+
+
 
 export function main() {
   describe('clia variant reports ntc component', () => {
     // Setting module for testing
     // Disable old forms
-
+    let config: any[] = [
+      { path: 'clia_variant_reports_ntc', component: 'CliaVariantReportsNtcComponent' }
+    ];
     beforeEach(() => {
       TestBed.configureTestingModule({
-        declarations: [TestComponent],
-        imports: [CliaVariantReportsNtcModule]
+        imports: [RouterTestingModule.withRoutes(config), DirectivesModule, PipesModule],
+        declarations: [CliaVariantReportsNtcComponent],
+        providers: [
+          { provide: CliaApiService, useClass: MockCliaApiService },
+          { provide: ActivatedRoute, useValue: { snapshot: { url: [{ path: 'vc_mocha' }] } } }
+        ]
       });
     });
 
-    // it('should work',
-    //   async(() => {
-    //     TestBed
-    //       .compileComponents()
-    //       .then(() => {
-    //         let fixture = TestBed.createComponent(TestComponent);
-    //         let aboutDOMEl = fixture.debugElement.children[0].nativeElement;
-
-    //       });
-    //   }));
+    it('should work',
+      async(() => {
+        TestBed
+          .compileComponents()
+          .then(() => {
+            let fixture = TestBed.overrideComponent(CliaVariantReportsNtcComponent, {
+              set: {
+                templateUrl: ''
+              }
+            }).createComponent(CliaVariantReportsNtcComponent);
+            // console.log(fixture);
+            fixture.componentInstance.ngOnInit();
+          });
+      }));
 
   });
 }
 
-@Component({
-  selector: 'test-cmp',
-  template: '<sd-clia-variant-reports-ntc></sd-clia-variant-reports-ntc>'
-})
-class TestComponent { }
+
+class MockCliaApiService {
+  getCliaVariantReportsNTC(): Observable<CliaVariantReportsNTCInterface> {
+    let testData: CliaVariantReportsNTCInterface;
+    testData = {
+      copy_number_variants: ['test'],
+      gene_fusions: ['test'],
+      snv_indels: ['test'],
+      molecular_id: { 'test': 'test' },
+      analysis_id: { 'test': 'test' },
+      total_variants: { 'test': 'test' },
+      mapd: { 'test': 'test' },
+      cellularity: { 'test': 'test' },
+      date_variant_received: { 'test': 'test' },
+      torrent_variant_caller_version: { 'test': 'test' },
+      report_status: { 'test': 'test' }
+    };
+    return Observable.of(testData);
+  }
+}
--- src/client/app/clia/clia-variant-reports-pacc/clia-variant-reports-pacc.component.spec.ts
+++ src/client/app/clia/clia-variant-reports-pacc/clia-variant-reports-pacc.component.spec.ts
@@ -1,39 +1,74 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
 import {
   async,
   TestBed
 } from '@angular/core/testing';
+import { Observable } from 'rxjs/Observable';
 
-import { CliaVariantReportsPaccModule } from './clia-variant-reports-pacc.module';
+// import { CliaVariantReportsPaccModule } from './clia-variant-reports-pacc.module';
+import { CliaVariantReportsPaccComponent } from './clia-variant-reports-pacc.component';
+// import { CliaVariantReportsPaccRoutingModule } from './clia-variant-reports-pacc-routing.module';
+import { CliaApiService, CliaVariantReportsPACCInterface } from './../clia-api.service';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { PipesModule } from './../../shared/pipes/pipes.module';
+import { DirectivesModule } from './../../shared/directives/directives.module';
+
+
 
 export function main() {
   describe('clia variant reports pacc component', () => {
     // Setting module for testing
     // Disable old forms
-
+    let config: any[] = [
+      { path: 'clia_variant_reports_pacc', component: 'CliaVariantReportsPaccComponent' }
+    ];
     beforeEach(() => {
       TestBed.configureTestingModule({
-        declarations: [TestComponent],
-        imports: [CliaVariantReportsPaccModule]
+        imports: [RouterTestingModule.withRoutes(config), DirectivesModule, PipesModule],
+        declarations: [CliaVariantReportsPaccComponent],
+        providers: [
+          { provide: CliaApiService, useClass: MockCliaApiService },
+          { provide: ActivatedRoute, useValue: { snapshot: { url: [{ path: 'vc_mocha' }] } } }
+        ]
       });
     });
 
-    // it('should work',
-    //   async(() => {
-    //     TestBed
-    //       .compileComponents()
-    //       .then(() => {
-    //         let fixture = TestBed.createComponent(TestComponent);
-    //         let aboutDOMEl = fixture.debugElement.children[0].nativeElement;
-
-    //       });
-    //   }));
+    it('should work',
+      async(() => {
+        TestBed
+          .compileComponents()
+          .then(() => {
+            let fixture = TestBed.overrideComponent(CliaVariantReportsPaccComponent, {
+              set: {
+                templateUrl: ''
+              }
+            }).createComponent(CliaVariantReportsPaccComponent);
+            // console.log(fixture);
+            fixture.componentInstance.ngOnInit();
+          });
+      }));
 
   });
 }
 
-@Component({
-  selector: 'test-cmp',
-  template: '<sd-clia-variant-reports-pacc></sd-clia-variant-reports-pacc>'
-})
-class TestComponent { }
+
+class MockCliaApiService {
+  getCliaVariantReportsPACC(): Observable<CliaVariantReportsPACCInterface> {
+    let testData: CliaVariantReportsPACCInterface;
+    testData = {
+      copy_number_variants: ['test'],
+      gene_fusions: ['test'],
+      snv_indels: ['test'],
+      molecular_id: { 'test': 'test' },
+      analysis_id: { 'test': 'test' },
+      total_variants: { 'test': 'test' },
+      mapd: { 'test': 'test' },
+      cellularity: { 'test': 'test' },
+      date_variant_received: { 'test': 'test' },
+      torrent_variant_caller_version: { 'test': 'test' },
+      report_status: { 'test': 'test' }
+    };
+    return Observable.of(testData);
+  }
+}
