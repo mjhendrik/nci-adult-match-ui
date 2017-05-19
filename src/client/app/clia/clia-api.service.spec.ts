25cdce8513da1767fa93ba8c551d15e5313c90a0
Subhash Vallala <sgvallala@gmail.com>
2017-05-19 09:19:38 -0400

    Unit tests

--- src/client/app/clia/clia-api.service.spec.ts
+++ src/client/app/clia/clia-api.service.spec.ts
@@ -23,42 +23,55 @@ export function main() {
             TestBed.configureTestingModule({
                 imports: [HttpModule],
                 providers: [
+                    MockBackend,
+                    BaseRequestOptions,
                     { provide: XHRBackend, useClass: MockBackend },
                     {
                         provide: Http,
                         useFactory: (backend: MockBackend, options: BaseRequestOptions) => new Http(backend, options),
                         deps: [MockBackend, BaseRequestOptions]
                     },
+                    { provide: AuthHttp, useExisting: Http },
                     CliaApiService
                 ]
             });
         });
 
-        // it('should return observable',
-        //     async(
-        //         inject([CliaApiService, XHRBackend],
-        //             (CliaApiService: CliaApiService, mockBackend: MockBackend) => {
+        it('should return observable',
+            inject([CliaApiService, XHRBackend],
+                (CliaApiService: CliaApiService, mockBackend: MockBackend) => {
 
-        //                 const mockResponse = {
-        //                     data: [
-        //                         { id: 0, name: 'Data 0' },
-        //                         { id: 1, name: 'Data 1' },
-        //                         { id: 2, name: 'Data 2' },
-        //                         { id: 3, name: 'Data 3' },
-        //                     ]
-        //                 };
+                    const mockResponse = {
+                        data: [
+                            { id: 0, name: 'Data 0' },
+                            { id: 1, name: 'Data 1' },
+                            { id: 2, name: 'Data 2' },
+                            { id: 3, name: 'Data 3' },
+                        ]
+                    };
 
-        //                 mockBackend.connections.subscribe((connection: any) => {
-        //                     connection.mockRespond(new Response(new ResponseOptions({
-        //                         body: JSON.stringify(mockResponse)
-        //                     })));
-        //                 });
+                    mockBackend.connections.subscribe((connection: any) => {
+                        connection.mockRespond(new Response(new ResponseOptions({
+                            body: mockResponse
+                        })));
+                    });
 
-        //                 CliaApiService.downloadCliaDnaBai('1234').subscribe((response: any) => {
-        //                     console.log(response);
-        //                 });
-        //             })
-        //     ));
+                    CliaApiService.downloadCliaDnaBai('1234').subscribe((response: any) => { });
+                    CliaApiService.downloadCliaDnaBam('1111');
+                    CliaApiService.downloadCliaRnaBai('1111');
+                    CliaApiService.downloadCliaRnaBam('1111');
+                    CliaApiService.downloadCliaVcf('1111');
+                    CliaApiService.generateMsn('test', 'test');
+                    CliaApiService.getCliaDetailsNTC('test');
+                    CliaApiService.getCliaDetailsPACC('test');
+                    CliaApiService.getCliaDetailsPC('test');
+                    CliaApiService.getCliaIon('test');
+                    CliaApiService.getCliaVariantReportQC('1111');
+                    CliaApiService.getCliaVariantReportsNTC('1111');
+                    CliaApiService.getCliaVariantReportsPACC('1111');
+                    CliaApiService.getCliaVariantReportsPC('1111');
+                })
+        );
 
     });
 }
