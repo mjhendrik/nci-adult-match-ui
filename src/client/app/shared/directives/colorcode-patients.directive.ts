fc481812c735d47bb348b7c280052acff23dcbc6
Subhash Vallala <sgvallala@gmail.com>
2017-05-30 14:15:19 -0400

    Patient status colorcode changes

--- src/client/app/shared/directives/colorcode-patients.directive.ts
+++ src/client/app/shared/directives/colorcode-patients.directive.ts
@@ -12,9 +12,8 @@ export class ColorCodePatientsDirective implements OnInit {
             this.el.nativeElement.classList.add('text-success-light');
         } else if (this.colorCodePatients === 'REGISTRATION') {
             this.el.nativeElement.classList.add('text-info-light');
-        } else if (this.colorCodePatients === 'COMPASSIONATE_CARE' || this.colorCodePatients === 'PENDING_APPROVAL'
-            || this.colorCodePatients === 'PENDING_CONFIRMATION' || this.colorCodePatients === 'REJOIN_REQUESTED'
-            || this.colorCodePatients === 'NOT_ELIGIBLE') {
+        } else if (this.colorCodePatients === 'COMPASSIONATE_CARE' || this.colorCodePatients.indexOf('PENDING_') !== -1
+            || this.colorCodePatients === 'REJOIN_REQUESTED' || this.colorCodePatients === 'NOT_ELIGIBLE') {
             this.el.nativeElement.classList.add('text-purple-light');
         } else if (this.colorCodePatients.indexOf('OFF_TRIAL_') !== -1 || this.colorCodePatients === 'OFF_TRIAL') {
             this.el.nativeElement.classList.add('text-danger-light');
