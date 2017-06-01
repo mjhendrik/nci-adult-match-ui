3fd893bcfc438c944ef3fd71d26a365f2b6080b3
Subhash Vallala <sgvallala@gmail.com>
2017-06-01 16:09:59 -0400

    Changed tab space from 4 to 2

--- src/client/app/clia/clia-api.service.spec.ts
+++ src/client/app/clia/clia-api.service.spec.ts
@@ -1,77 +1,77 @@
 import { Component } from '@angular/core';
 import {
-    async,
-    TestBed,
-    inject
+  async,
+  TestBed,
+  inject
 } from '@angular/core/testing';
 import { Observable } from 'rxjs/Observable';
 import { CliaApiService } from './clia-api.service';
 import {
-    HttpModule,
-    Http,
-    Response,
-    ResponseOptions,
-    XHRBackend,
-    BaseRequestOptions
+  HttpModule,
+  Http,
+  Response,
+  ResponseOptions,
+  XHRBackend,
+  BaseRequestOptions
 } from '@angular/http';
 import { MockBackend } from '@angular/http/testing';
 import { AuthHttp } from 'angular2-jwt';
 
 export function main() {
-    describe('clia api service test', () => {
-        beforeEach(() => {
-            TestBed.configureTestingModule({
-                imports: [HttpModule],
-                providers: [
-                    MockBackend,
-                    BaseRequestOptions,
-                    { provide: XHRBackend, useClass: MockBackend },
-                    {
-                        provide: Http,
-                        useFactory: (backend: MockBackend, options: BaseRequestOptions) => new Http(backend, options),
-                        deps: [MockBackend, BaseRequestOptions]
-                    },
-                    { provide: AuthHttp, useExisting: Http },
-                    CliaApiService
-                ]
-            });
-        });
-
-        it('should return observable',
-            inject([CliaApiService, XHRBackend],
-                (CliaApiService: CliaApiService, mockBackend: MockBackend) => {
-
-                    const mockResponse = {
-                        data: [
-                            { id: 0, name: 'Data 0' },
-                            { id: 1, name: 'Data 1' },
-                            { id: 2, name: 'Data 2' },
-                            { id: 3, name: 'Data 3' },
-                        ]
-                    };
-
-                    mockBackend.connections.subscribe((connection: any) => {
-                        connection.mockRespond(new Response(new ResponseOptions({
-                            body: mockResponse
-                        })));
-                    });
-
-                    CliaApiService.downloadCliaDnaBai('1234').subscribe((response: any) => { });
-                    CliaApiService.downloadCliaDnaBam('1111');
-                    CliaApiService.downloadCliaRnaBai('1111');
-                    CliaApiService.downloadCliaRnaBam('1111');
-                    CliaApiService.downloadCliaVcf('1111');
-                    CliaApiService.generateMsn('test', 'test');
-                    CliaApiService.getCliaDetailsNTC('test');
-                    CliaApiService.getCliaDetailsPACC('test');
-                    CliaApiService.getCliaDetailsPC('test');
-                    CliaApiService.getCliaIon('test');
-                    CliaApiService.getCliaVariantReportQC('1111');
-                    CliaApiService.getCliaVariantReportsNTC('1111');
-                    CliaApiService.getCliaVariantReportsPACC('1111');
-                    CliaApiService.getCliaVariantReportsPC('1111');
-                })
-        );
-
+  describe('clia api service test', () => {
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [HttpModule],
+        providers: [
+          MockBackend,
+          BaseRequestOptions,
+          { provide: XHRBackend, useClass: MockBackend },
+          {
+            provide: Http,
+            useFactory: (backend: MockBackend, options: BaseRequestOptions) => new Http(backend, options),
+            deps: [MockBackend, BaseRequestOptions]
+          },
+          { provide: AuthHttp, useExisting: Http },
+          CliaApiService
+        ]
+      });
     });
+
+    it('should return observable',
+      inject([CliaApiService, XHRBackend],
+        (CliaApiService: CliaApiService, mockBackend: MockBackend) => {
+
+          const mockResponse = {
+            data: [
+              { id: 0, name: 'Data 0' },
+              { id: 1, name: 'Data 1' },
+              { id: 2, name: 'Data 2' },
+              { id: 3, name: 'Data 3' },
+            ]
+          };
+
+          mockBackend.connections.subscribe((connection: any) => {
+            connection.mockRespond(new Response(new ResponseOptions({
+              body: mockResponse
+            })));
+          });
+
+          CliaApiService.downloadCliaDnaBai('1234').subscribe((response: any) => { });
+          CliaApiService.downloadCliaDnaBam('1111');
+          CliaApiService.downloadCliaRnaBai('1111');
+          CliaApiService.downloadCliaRnaBam('1111');
+          CliaApiService.downloadCliaVcf('1111');
+          CliaApiService.generateMsn('test', 'test');
+          CliaApiService.getCliaDetailsNTC('test');
+          CliaApiService.getCliaDetailsPACC('test');
+          CliaApiService.getCliaDetailsPC('test');
+          CliaApiService.getCliaIon('test');
+          CliaApiService.getCliaVariantReportQC('1111');
+          CliaApiService.getCliaVariantReportsNTC('1111');
+          CliaApiService.getCliaVariantReportsPACC('1111');
+          CliaApiService.getCliaVariantReportsPC('1111');
+        })
+    );
+
+  });
 }
