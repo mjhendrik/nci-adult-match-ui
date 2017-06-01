3702750c2c36b6d9180d35878897dc0bbb9283b7
Subhash Vallala <sgvallala@gmail.com>
2017-06-01 11:08:36 -0400

    Changing env config

--- tools/env/test.ts
+++ tools/env/test.ts
@@ -2,9 +2,9 @@ import { EnvConfig } from './env-config.interface';
 
 const TestConfig: EnvConfig = {
   API: {
-    PATIENT: 'https://***REMOVED***/api/v1',
-    TREATMENT_ARM: 'https://***REMOVED***/api/v1',
-    ION_REPORTER: 'https://***REMOVED***/api/v1'
+    PATIENT: 'https://match-int.nci.nih.gov/api/v1',
+    TREATMENT_ARM: 'https://match-int.nci.nih.gov/api/v1',
+    ION_REPORTER: 'https://match-int.nci.nih.gov/api/v1'
   },
   CLIENT_ID: 'S2nuyxIldVc6qG7360ZTT2Er0IQA3AlX', // IntTest
   AUTH_DOMAIN: 'ncimatch.auth0.com'
