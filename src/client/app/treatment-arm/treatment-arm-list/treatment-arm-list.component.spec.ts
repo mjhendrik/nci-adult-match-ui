69e7caa332d9832dffa1b6c80196fa8159b5eda7
Subhash Vallala <sgvallala@gmail.com>
2017-06-01 16:10:08 -0400

    Added TA unit tests

--- src/client/app/treatment-arm/treatment-arm-list/treatment-arm-list.component.spec.ts
+++ src/client/app/treatment-arm/treatment-arm-list/treatment-arm-list.component.spec.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { async, TestBed } from '@angular/core/testing';
+import {
+  async,
+  TestBed
+} from '@angular/core/testing';
 
 import { TreatmentArmListModule } from './treatment-arm-list.module';
 
