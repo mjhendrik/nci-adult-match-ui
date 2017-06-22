2d0d35191933febf07cd9a86a868c6d7ed0f8118
Subhash Vallala <sgvallala@gmail.com>
2017-06-22 13:27:19 -0400

    Adding biopsy unit tests

---
+++ src/client/app/biopsy-tracking/biopsy-tracking-api.service.spec.ts
@@ -0,0 +1,64 @@
+import { Component } from '@angular/core';
+import {
+  async,
+  TestBed,
+  inject
+} from '@angular/core/testing';
+import { Observable } from 'rxjs/Observable';
+import { BiopsyTrackingApiService } from './biopsy-tracking-api.service';
+import {
+  HttpModule,
+  Http,
+  Response,
+  ResponseOptions,
+  XHRBackend,
+  BaseRequestOptions
+} from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
+import { AuthHttp } from 'angular2-jwt';
+
+export function main() {
+  describe('biopsy tracking api service test', () => {
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [HttpModule],
+        providers: [
+          MockBackend,
+          BaseRequestOptions,
+          { provide: XHRBackend, useClass: MockBackend },
+          {
+            provide: Http,
+            useFactory: (backend: MockBackend, options: BaseRequestOptions) => new Http(backend, options),
+            deps: [MockBackend, BaseRequestOptions]
+          },
+          { provide: AuthHttp, useExisting: Http },
+          BiopsyTrackingApiService
+        ]
+      });
+    });
+
+    it('should return observable',
+      inject([BiopsyTrackingApiService, XHRBackend],
+        (BiopsyTrackingApiService: BiopsyTrackingApiService, mockBackend: MockBackend) => {
+
+          const mockResponse = {
+            data: [
+              { id: 0, name: 'Data 0' },
+              { id: 1, name: 'Data 1' },
+              { id: 2, name: 'Data 2' },
+              { id: 3, name: 'Data 3' },
+            ]
+          };
+
+          mockBackend.connections.subscribe((connection: any) => {
+            connection.mockRespond(new Response(new ResponseOptions({
+              body: mockResponse
+            })));
+          });
+
+          BiopsyTrackingApiService.getBiopsyTracking().subscribe((response: any) => { });
+        })
+    );
+
+  });
+}
--- src/client/app/treatment-arm/treatment-arm-api.service.spec.ts
+++ src/client/app/treatment-arm/treatment-arm-api.service.spec.ts
@@ -1,67 +1,67 @@
 import { Component } from '@angular/core';
 import {
-    async,
-    TestBed,
-    inject
+  async,
+  TestBed,
+  inject
 } from '@angular/core/testing';
 import { Observable } from 'rxjs/Observable';
 import { TreatmentArmApiService } from './treatment-arm-api.service';
 import {
-    HttpModule,
-    Http,
-    Response,
-    ResponseOptions,
-    XHRBackend,
-    BaseRequestOptions
+  HttpModule,
+  Http,
+  Response,
+  ResponseOptions,
+  XHRBackend,
+  BaseRequestOptions
 } from '@angular/http';
 import { MockBackend } from '@angular/http/testing';
 import { AuthHttp } from 'angular2-jwt';
 
 export function main() {
-    describe('treatment arm api service test', () => {
-        beforeEach(() => {
-            TestBed.configureTestingModule({
-                imports: [HttpModule],
-                providers: [
-                    MockBackend,
-                    BaseRequestOptions,
-                    { provide: XHRBackend, useClass: MockBackend },
-                    {
-                        provide: Http,
-                        useFactory: (backend: MockBackend, options: BaseRequestOptions) => new Http(backend, options),
-                        deps: [MockBackend, BaseRequestOptions]
-                    },
-                    { provide: AuthHttp, useExisting: Http },
-                    TreatmentArmApiService
-                ]
-            });
-        });
-
-        it('should return observable',
-            inject([TreatmentArmApiService, XHRBackend],
-                (TreatmentArmApiService: TreatmentArmApiService, mockBackend: MockBackend) => {
-
-                    const mockResponse = {
-                        data: [
-                            { id: 0, name: 'Data 0' },
-                            { id: 1, name: 'Data 1' },
-                            { id: 2, name: 'Data 2' },
-                            { id: 3, name: 'Data 3' },
-                        ]
-                    };
-
-                    mockBackend.connections.subscribe((connection: any) => {
-                        connection.mockRespond(new Response(new ResponseOptions({
-                            body: mockResponse
-                        })));
-                    });
-
-                    TreatmentArmApiService.getTreatmentArmDetails('1234').subscribe((response: any) => { });
-                    TreatmentArmApiService.getPreviousTreatmentArmDetails('1111');
-                    TreatmentArmApiService.getTreatmentArmVersions('1111');
-                    TreatmentArmApiService.getTreatmentArmList();
-                })
-        );
-
+  describe('treatment arm api service test', () => {
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [HttpModule],
+        providers: [
+          MockBackend,
+          BaseRequestOptions,
+          { provide: XHRBackend, useClass: MockBackend },
+          {
+            provide: Http,
+            useFactory: (backend: MockBackend, options: BaseRequestOptions) => new Http(backend, options),
+            deps: [MockBackend, BaseRequestOptions]
+          },
+          { provide: AuthHttp, useExisting: Http },
+          TreatmentArmApiService
+        ]
+      });
     });
+
+    it('should return observable',
+      inject([TreatmentArmApiService, XHRBackend],
+        (TreatmentArmApiService: TreatmentArmApiService, mockBackend: MockBackend) => {
+
+          const mockResponse = {
+            data: [
+              { id: 0, name: 'Data 0' },
+              { id: 1, name: 'Data 1' },
+              { id: 2, name: 'Data 2' },
+              { id: 3, name: 'Data 3' },
+            ]
+          };
+
+          mockBackend.connections.subscribe((connection: any) => {
+            connection.mockRespond(new Response(new ResponseOptions({
+              body: mockResponse
+            })));
+          });
+
+          TreatmentArmApiService.getTreatmentArmDetails('1234').subscribe((response: any) => { });
+          TreatmentArmApiService.getPreviousTreatmentArmDetails('1111');
+          TreatmentArmApiService.getTreatmentArmVersions('1111');
+          TreatmentArmApiService.getTreatmentArmList();
+        })
+    );
+
+  });
 }
