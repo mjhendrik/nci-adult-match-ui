7b4f694897d0f1cea71f4558f7c86e6954c33f71
Subhash Vallala <sgvallala@gmail.com>
2017-06-26 10:08:16 -0400

    Added navbar hide/show transition

--- src/client/app/shared/navbar/navbar.component.ts
+++ src/client/app/shared/navbar/navbar.component.ts
@@ -5,11 +5,13 @@ import {
 } from '@angular/common';
 import {
   Component,
-  OnInit
+  Inject,
+  OnInit,
+  HostListener
 } from '@angular/core';
 import { Auth } from './../auth/auth.service';
 import { ConfigApiService } from './../config/config-api.service';
-
+import { DOCUMENT } from '@angular/platform-browser';
 /**
  * This class represents the navigation bar component.
  */
@@ -25,9 +27,16 @@ export class NavbarComponent implements OnInit {
   userName: string;
 
   location: Location;
+
+  didScroll: any;
+  lastScrollTop = 0;
+  delta = 5;
+  navbarheight: any;
+
   constructor(location: Location,
     private auth: Auth,
-    private configApi: ConfigApiService) {
+    private configApi: ConfigApiService,
+    @Inject(DOCUMENT) private document: Document) {
     this.location = location;
   }
 
@@ -52,10 +61,63 @@ export class NavbarComponent implements OnInit {
     return true;
   }
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+
+    this.navbarheight = document.getElementById('top-menu').offsetHeight;
+    this.didScroll = true;
+
+    setTimeout(() => {
+      if (this.didScroll) {
+        this.hasScrolled();
+        this.didScroll = false;
+      }
+    }, 250);
+
+  }
+
+  hasScrolled() {
+
+    var st: any = (document.body.scrollTop).toFixed();
+
+    // Make sure they scroll more than delta
+    if (Math.abs(this.lastScrollTop - st) <= this.delta)
+      return;
+
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is 'behind' the navbar.
+
+    if (st > this.lastScrollTop && st > this.navbarheight) {
+
+      // Scroll Down
+      document.getElementById('top-menu').classList.remove('top-position');
+      document.getElementById('header').classList.add('header-up');
+      document.getElementById('top-menu').classList.add('top-menu-up');
+      document.getElementById('maindiv').classList.add('header-up');
+
+    } else {
+
+      // Scroll Up
+      if ((parseInt(st) + window.innerHeight) < document.body.scrollHeight) {
+
+        document.getElementById('header').classList.remove('header-up');
+        document.getElementById('top-menu').classList.remove('top-menu-up');
+        document.getElementById('top-menu').classList.add('top-position');
+        document.getElementById('maindiv').classList.remove('header-up');
+
+      }
+
+    }
+
+    this.lastScrollTop = st;
+
+  }
+
   getData() {
     this.configApi.getBuildInfo()
       .subscribe(info => {
         this.buildInfo = info;
       });
   }
+
 }
