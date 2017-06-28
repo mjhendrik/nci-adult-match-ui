82679a8bdc649289d866401f128d1824ace52b3e
Subhash Vallala <sgvallala@gmail.com>
2017-06-28 15:05:59 -0400

    Patient-list changes

--- src/client/app/shared/directives/colorcode-patients.directive.ts
+++ src/client/app/shared/directives/colorcode-patients.directive.ts
@@ -14,14 +14,14 @@ export class ColorCodePatientsDirective implements OnInit {
 
         if (this.colorCodePatients === 'ON_TREATMENT_ARM') {
             this.el.nativeElement.classList.add('text-success-light');
-        } else if (this.colorCodePatients === 'REGISTRATION') {
+        } else if (this.colorCodePatients === 'REGISTRATION' || this.colorCodePatients === 'REGISTRATION_OUTSIDE_ASSAY') {
             this.el.nativeElement.classList.add('text-info-light');
         } else if (this.colorCodePatients === 'COMPASSIONATE_CARE' || this.colorCodePatients.indexOf('PENDING_') !== -1
             || this.colorCodePatients === 'REJOIN_REQUESTED' || this.colorCodePatients === 'NOT_ELIGIBLE') {
             this.el.nativeElement.classList.add('text-purple-light');
         } else if (this.colorCodePatients.indexOf('OFF_TRIAL_') !== -1 || this.colorCodePatients === 'OFF_TRIAL') {
             this.el.nativeElement.classList.add('text-danger-light');
-        } else if (this.colorCodePatients === 'FORMERLY_ON_ARM_OFF_TRIAL') {
+        } else if (this.colorCodePatients === 'FORMERLY_ON_ARM_OFF_TRIAL' || this.colorCodePatients === 'PROGRESSION_REBIOPSY') {
             this.el.nativeElement.classList.add('text-warning-light');
         }
 
