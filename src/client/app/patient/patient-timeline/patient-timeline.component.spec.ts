dab70115b131db9ebc90e96158dc02f312dcf633
Subhash Vallala <sgvallala@gmail.com>
2017-07-16 18:14:35 -0400

    Patient timeline unit tests

--- src/client/app/patient/patient-timeline/patient-timeline.component.spec.ts
+++ src/client/app/patient/patient-timeline/patient-timeline.component.spec.ts
@@ -1,38 +1,38 @@
-import { Component } from '@angular/core';
 import {
-  async,
-  TestBed
+  TestBed,
+  async
 } from '@angular/core/testing';
-
-import { PatientTimelineModule } from './patient-timeline.module';
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { RouterTestingModule } from '@angular/router/testing';
+import { DirectivesModule } from "./../../shared/directives/directives.module";
+import { PipesModule } from "./../../shared/pipes/pipes.module";
+import { LongListModule } from '../../shared/long-list/long-list.module';
+import { PatientTimelineComponent } from './patient-timeline.component';
 
 export function main() {
-  describe('patient-timeline component', () => {
-    // Setting module for testing
-    // Disable old forms
+  describe('patient timeline component', () => {
 
     beforeEach(() => {
       TestBed.configureTestingModule({
-        declarations: [TestComponent],
-        imports: [PatientTimelineModule]
+        imports: [RouterTestingModule, CommonModule, PipesModule, FormsModule, DirectivesModule,
+          LongListModule],
+        declarations: [PatientTimelineComponent]
       });
     });
 
-    // it('should work',
-    //   async(() => {
-    //     TestBed
-    //       .compileComponents()
-    //       .then(() => {
-    //         let fixture = TestBed.createComponent(TestComponent);
-    //         let aboutDOMEl = fixture.debugElement.children[0].nativeElement;
-    //       });
-    //   }));
-
+    it('should test getIcon',
+      async((done: any) => {
+        TestBed
+          .compileComponents()
+          .then(() => {
+            let fixture = TestBed.overrideComponent(PatientTimelineComponent, {
+              set: {
+                templateUrl: ''
+              }
+            }).createComponent(PatientTimelineComponent);
+            fixture.componentInstance.getIcon("test");
+          });
+      }));
   });
 }
-
-@Component({
-  selector: 'test-cmp',
-  template: '<sd-patient-timeline></sd-patient-timeline>'
-})
-class TestComponent { }
