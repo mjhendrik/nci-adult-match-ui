0b5ffcb3d0c379cb055b683c6f413aca30fd0051
Subhash Vallala <sgvallala@gmail.com>
2017-07-16 23:41:03 -0400

    Patient QCVR unit tests

--- src/client/app/patient/patient-variant-report-qc/patient-variant-report-qc.component.spec.ts
+++ src/client/app/patient/patient-variant-report-qc/patient-variant-report-qc.component.spec.ts
@@ -1,39 +1,149 @@
 import { Component } from '@angular/core';
 import {
   async,
-  TestBed
+  TestBed,
+  ComponentFixture
 } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { FormsModule } from "@angular/forms";
+import { Observable } from "rxjs/Observable";
+import { RouterTestingModule } from '@angular/router/testing';
+import { DirectivesModule } from "./../../shared/directives/directives.module";
+import { PipesModule } from "./../../shared/pipes/pipes.module";
+import { DataTableModule } from "./../../shared/datatables/DataTableModule";
+import { PatientVariantReportQcComponent } from './patient-variant-report-qc.component';
+import { PatientApiService } from "./../patient-api.service";
+import { VariantReportFilteredTableModule } from '../../shared/variant-report-filtered-table/variant-report-filtered-table.module';
+import { CommonModule } from '@angular/common';
 
-import { PatientVariantReportQcModule } from './patient-variant-report-qc.module';
+
 
 export function main() {
-  describe('patient-variant-report-qc component', () => {
+  describe('patient varient report qc component', () => {
     // Setting module for testing
     // Disable old forms
 
+    let config: any[] = [
+      { path: 'patients', component: 'PatientVariantReportQcComponent' }
+    ];
+
     beforeEach(() => {
       TestBed.configureTestingModule({
-        declarations: [TestComponent],
-        imports: [PatientVariantReportQcModule]
+        imports: [RouterTestingModule.withRoutes(config), DirectivesModule,
+          PipesModule, FormsModule, DataTableModule, CommonModule,
+          VariantReportFilteredTableModule],
+        providers: [
+          { provide: PatientApiService, useClass: MockPatientApiService },
+          { provide: ActivatedRoute, useValue: { snapshot: { params: { patientSequenceNumber: 1067, analysisId: 1234 } } } }
+        ]
       });
     });
 
-    // it('should work',
-    //   async(() => {
+    // it('should test ngOnInit',
+    //   async((done: any) => {
     //     TestBed
     //       .compileComponents()
     //       .then(() => {
-    //         let fixture = TestBed.createComponent(TestComponent);
-    //         let aboutDOMEl = fixture.debugElement.children[0].nativeElement;
-
+    //         let fixture = TestBed.overrideComponent(PatientVariantReportQcComponent, {
+    //           set: {
+    //             templateUrl: ''
+    //           }
+    //         }).createComponent(PatientVariantReportQcComponent);
+    //         //fixture.componentInstance.ngOnInit();
     //       });
     //   }));
 
   });
+
+
+  // describe('patients component', () => {
+  //   // Setting module for testing
+  //   // Disable old forms
+
+  //   let config: any[] = [
+  //     { path: 'patient', component: 'PatientVariantReportQcComponent' }
+  //   ];
+
+  //   beforeEach(() => {
+  //     TestBed.configureTestingModule({
+  //       imports: [RouterTestingModule.withRoutes(config), DirectivesModule,
+  //         PipesModule, FormsModule, DataTableModule, CommonModule,
+  //         VariantReportFilteredTableModule],
+  //       declarations: [PatientVariantReportQcComponent],
+  //       providers: [
+  //         { provide: PatientApiService, useClass: MockPatientApiServiceError },
+  //         { provide: ActivatedRoute, useValue: { snapshot: { params: { patientSequenceNumber: 1067, analysisId: 1234 } } } }
+  //       ]
+  //     });
+  //   });
+
+  //   it('should test getData',
+  //     async((done: any) => {
+  //       TestBed
+  //         .compileComponents()
+  //         .then(() => {
+  //           let fixture = TestBed.overrideComponent(PatientVariantReportQcComponent, {
+  //             set: {
+  //               templateUrl: ''
+  //             }
+  //           }).createComponent(PatientVariantReportQcComponent);
+  //           fixture.componentInstance.getData('1067', '1234');
+  //         });
+
+  //     }));
+  // });
+
 }
 
-@Component({
-  selector: 'test-cmp',
-  template: '<sd-patient-variant-report-qc></sd-patient-variant-report-qc>'
-})
-class TestComponent { }
+
+class MockPatientApiServiceError {
+
+  getPatientVariantReportQc(psn: any): Observable<any> {
+    return Observable.throw("error");
+  }
+}
+
+class MockPatientApiService {
+
+  getPatientVariantReportQc(psn: any): Observable<any> {
+    let testdata = {
+      molecularSequenceNumber: "1234",
+      dateReceived: "",
+      copy_number_variants: "",
+      gene_fusions: "",
+      indels: "",
+      single_nucleotide_variants: ""
+    };
+
+    return Observable.of(testdata);
+  }
+
+  getPatientVariantReportOcp(): Observable<any> {
+    return Observable.of({
+      tvc_version: '',
+      pool1: '',
+      pool2: '',
+      biopsySequenceNumber: '',
+      genes: ''
+    });
+  }
+
+  getPatientCopyNumberReport(): Observable<any> {
+    return Observable.of({
+      mapd: "",
+      cellularity: '',
+      parsed_vcf_genes: '',
+      dnaBamFilePath: '',
+      rnaBamFilePath: '',
+      vcfFilePath: ''
+    });
+
+  }
+  getPatientVariantReportFileInfo(): Observable<any> {
+    return Observable.of({
+      dnaBamFilePath: '',
+      rnaBamFilePath: '',
+      vcfFilePath: ''
+    });
+  }
+}
