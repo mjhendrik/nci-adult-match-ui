b0ca8b10966c1a7a59f63a02f2c1fb4b80876e2b
Rick Zakharov <rick.zakharov@gmail.com>
2017-07-20 16:22:19 -0400

    Added mechanism to exlude some urls from automatic http error handling

--- src/client/app/shared/http.interceptor.ts
+++ src/client/app/shared/http.interceptor.ts
@@ -11,9 +11,31 @@ import {
 } from '@angular/http';
 
 import { Observable } from 'rxjs/Rx';
+import { Config } from './config/env.config';
+
+const anyParamRe = '([-a-zA-Z0-9:%_\+.~#?&//=]+)';
+const exlcudedFromHandlingUrlPatterns = [
+    RegExp(Config.API.PATIENT + '/patients/' + anyParamRe + '/variant_reports/' + anyParamRe + '/copy_number_report')
+];
 
 @Injectable()
 export class HttpInterceptor extends Http {
+    excludedFromErrorHandling(url: string): any {
+        if (!url)
+            return false;
+
+        try {
+            for (const re of exlcudedFromHandlingUrlPatterns) {
+                if (re.test(url)) {
+                    return true;
+                }
+            }
+        } catch (error) {
+            return false;            
+        }
+
+        return false;
+    }
 
     constructor(backend: ConnectionBackend, defaultOptions: RequestOptions, public router: Router) {
         super(backend, defaultOptions);
@@ -25,7 +47,7 @@ export class HttpInterceptor extends Http {
 
     responseError() {
         return (response: Response) => {
-            if (response.status === 404) {
+            if (response.status === 404 && !this.excludedFromErrorHandling(response.url)) {
                 this.router.navigate(['/error']);
             }
             return Observable.throw(response);
