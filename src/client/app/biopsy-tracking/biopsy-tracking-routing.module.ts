3a4225b712bfab158298bfb19af9a9117f64b4c3
Subhash Vallala <sgvallala@gmail.com>
2017-07-25 10:24:18 -0400

    Improving biopsy-tracking data retrieval mechanisms

--- src/client/app/biopsy-tracking/biopsy-tracking-routing.module.ts
+++ src/client/app/biopsy-tracking/biopsy-tracking-routing.module.ts
@@ -1,15 +1,46 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import {
+  NgModule,
+  Injectable
+} from '@angular/core';
+import {
+  RouterModule,
+  Resolve,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot
+} from '@angular/router';
 
 import { BiopsyTrackingListComponent } from './biopsy-tracking.component';
 import { AuthGuard } from './../shared/auth/auth.guard.service';
+import { Observable } from 'rxjs/Observable';
+import { BiopsyTrackingApiService } from './biopsy-tracking-api.service';
+
+@Injectable()
+class DataResolver implements Resolve<any> {
+  constructor(private api: BiopsyTrackingApiService) { }
+
+  resolve(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<any> | Promise<any> | any {
+    return Observable.forkJoin(
+      this.api.getBiopsyCount('')
+    ).map(
+      data => {
+        return {
+          data: data[0]
+        };
+      }
+      );
+  }
+}
 
 @NgModule({
   imports: [
     RouterModule.forChild([
-      { path: 'tracking', component: BiopsyTrackingListComponent, canActivate: [AuthGuard] }
+      { path: 'tracking', component: BiopsyTrackingListComponent, canActivate: [AuthGuard], resolve: { data: DataResolver } }
     ])
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [DataResolver]
 })
 export class BiopsyTrackingListRoutingModule { }
