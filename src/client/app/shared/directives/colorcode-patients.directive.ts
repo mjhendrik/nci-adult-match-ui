e2e7b21c6f0f2b57027e310a5c800d30a61eac00
Subhash Vallala <sgvallala@gmail.com>
2017-08-16 15:29:26 -0400

    Working on dashboard

--- src/client/app/shared/directives/colorcode-patients.directive.ts
+++ src/client/app/shared/directives/colorcode-patients.directive.ts
@@ -33,6 +33,25 @@ export class ColorCodePatientsDirective extends ConditionalCssDirective<string> 
                         && (x.indexOf('FORMERLY') !== -1 || x.indexOf('PROGRESSION') !== -1),
                     cssClass: 'text-warning-light'
                 },
+                {
+                    evaluate: (x) => x
+                        && (x === 'TISSUE_SPECIMEN_RECEIVED'
+                            || x === 'TISSUE_NUCLEIC_ACID_SHIPPED'
+                            || x === 'TISSUE_SLIDE_SPECIMEN_SHIPPED'),
+                    cssClass: 'text-info'
+                },
+                { evaluate: (x) => x === 'ASSAY_RESULTS_RECEIVED', cssClass: 'text-success' },
+                { evaluate: (x) => x === 'TISSUE_VARIANT_REPORT_RECEIVED', cssClass: 'text-pink' },
+                {
+                    evaluate: (x) => x
+                        && (x === 'TISSUE_VARIANT_REPORT_REJECTED'
+                            || x === 'TISSUE_VARIANT_REPORT_CONFIRMED'
+                            || x === 'PENDING_CONFIRMATION'
+                            || x === 'PENDING_APPROVAL'
+                            || x === 'AWAITING_PATIENT_DATA'
+                            || x === 'AWAITING_TREATMENT_ARM_STATUS'),
+                    cssClass: 'text-orangeshade-dark'
+                },
             ]);
     }
 }
