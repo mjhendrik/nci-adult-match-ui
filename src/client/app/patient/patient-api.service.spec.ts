c9ae69849dfced55b3980695ff2d954566670dd8
Rick Zakharov <rick.zakharov@gmail.com>
2017-08-22 16:12:29 -0400

    Implementing unit tests.

--- src/client/app/patient/patient-api.service.spec.ts
+++ src/client/app/patient/patient-api.service.spec.ts
@@ -60,23 +60,23 @@ const makeOutsideAssayComparisonVariantReportData = () => {
     currentStepNumber: 'value',
     concordance: 'value',
     outsideData: {
-        analysisId: 'value',
-        assays: ['value'],
-        variantReport: 'value',
-        assignmentReport: 'value',
-        tvc_version: 'value',
-        pool1: 1,
-        pool2: 1,
-        mapd: 'value',
-        cellularity: 'value',
-        showPools: false
+      analysisId: 'value',
+      assays: ['value'],
+      variantReport: 'value',
+      assignmentReport: 'value',
+      tvc_version: 'value',
+      pool1: 1,
+      pool2: 1,
+      mapd: 'value',
+      cellularity: 'value',
+      showPools: false
     },
     matchData: {
       analysisId: 'value',
       assays: ['value'],
       variantReport: 'value',
       assignmentReport: 'value',
-    tvc_version: 'value',
+      tvc_version: 'value',
       pool1: 1,
       pool2: 1,
       mapd: 'value',
@@ -84,11 +84,32 @@ const makeOutsideAssayComparisonVariantReportData = () => {
       showPools: false
     },
     comparisonVariantReport: {
-        singleNucleotideVariantAndIndels: ['value'],
-        copyNumberVariants: ['value'],
-        unifiedGeneFusions: ['value']
+      singleNucleotideVariantAndIndels: ['value'],
+      copyNumberVariants: ['value'],
+      unifiedGeneFusions: ['value']
     },
-} as VariantReportComparisonData;
+  } as VariantReportComparisonData;
+};
+
+const makeVariantReportQcData = () => {
+  return {
+    variantReport: {
+      patientSequenceNumber: '11276',
+      patientStatus: 'PENDING_CONFIRMATION',
+      step: 0,
+      concordance: 'YES',
+      variantReportStatus: 'CONFIRMED',
+      variantReportDate: 'August 26, 2016 3:28 PM GMT',
+      user: 'TA commettee',
+      biopsySequenceNumber: 'T-16-000762',
+      molecularSequenceNumber: 'MSN17772',
+      analysisId: 'MSN17772_v1_92ad9833-e79a-4807-b1f3-6be88a0ab824',
+      mapd: '0.317',
+      cellularity: '1.000000',
+      fileReceivedDate: 'August 25, 2016 10:13 PM GMT',
+      torrentVariantCallerVersion: '5.0-9'
+    }
+  } as any;
 };
 
 export function main() {
@@ -433,7 +454,7 @@ export function main() {
     describe('when getOutsideAssayComparisonVariantReport', () => {
       let backend: MockBackend;
       let service: PatientApiService;
-      let fakeData: any;
+      let fakeData: VariantReportComparisonData;
       let response: Response;
 
       beforeEach(inject([AuthHttp, XHRBackend], (http: AuthHttp, be: MockBackend) => {
@@ -490,5 +511,194 @@ export function main() {
       })));
 
     });
+
+
+    describe('when getPatientVariantReportQc', () => {
+      let backend: MockBackend;
+      let service: PatientApiService;
+      let fakeData: any;
+      let response: Response;
+
+      beforeEach(inject([AuthHttp, XHRBackend], (http: AuthHttp, be: MockBackend) => {
+        backend = be;
+        service = new PatientApiService(http);
+        fakeData = makeVariantReportQcData();
+        let options = new ResponseOptions({ status: 200, body: fakeData });
+        response = new Response(options);
+      }));
+
+      it('should have expected fake patients (then)', async(inject([], () => {
+        backend.connections.subscribe((c: MockConnection) => c.mockRespond(response));
+
+        service.getPatientVariantReportQc('fake-psn', 'faka-analysis-id').toPromise()
+          .then(patient => {
+            expect(patient).toBe(fakeData);
+          });
+      })));
+
+      it('should have expected fake patients (Observable.do)', async(inject([], () => {
+        backend.connections.subscribe((c: MockConnection) => c.mockRespond(response));
+
+        service.getPatientVariantReportQc('fake-psn', 'faka-analysis-id')
+          .do(patient => {
+            expect(patient).toBe(fakeData, 'should have expected no. of patients');
+          })
+          .toPromise();
+      })));
+
+      it('should be OK returning no patients', async(inject([], () => {
+        let resp = new Response(new ResponseOptions({ status: 200, body: {} }));
+        backend.connections.subscribe((c: MockConnection) => c.mockRespond(resp));
+
+        service.getPatientVariantReportQc('fake-psn', 'faka-analysis-id')
+          .do(patient => {
+            expect(patient).toEqual({}, 'should have no patients');
+          })
+          .toPromise();
+      })));
+
+      it('should treat 404 as an Observable error', async(inject([], () => {
+        let resp = new Response(new ResponseOptions({ status: 404 }));
+        backend.connections.subscribe((c: MockConnection) => c.mockRespond(resp));
+
+        service.getPatientVariantReportQc('fake-psn', 'faka-analysis-id')
+          .do(patient => {
+            fail('should not respond with patients');
+          })
+          .catch(err => {
+            expect(err).toMatch(/Bad response status/, 'should catch bad response status code');
+            return Observable.of(null); // failure is the expected test result
+          })
+          .toPromise();
+      })));
+
+    });
+
+
+    describe('when getPatientVariantReportOcp', () => {
+      let backend: MockBackend;
+      let service: PatientApiService;
+      let fakeData: any;
+      let response: Response;
+
+      beforeEach(inject([AuthHttp, XHRBackend], (http: AuthHttp, be: MockBackend) => {
+        backend = be;
+        service = new PatientApiService(http);
+        fakeData = makeVariantReportQcData();
+        let options = new ResponseOptions({ status: 200, body: fakeData });
+        response = new Response(options);
+      }));
+
+      it('should have expected fake patients (then)', async(inject([], () => {
+        backend.connections.subscribe((c: MockConnection) => c.mockRespond(response));
+
+        service.getPatientVariantReportOcp('fake-psn', 'faka-analysis-id').toPromise()
+          .then(patient => {
+            expect(patient).toBe(fakeData);
+          });
+      })));
+
+      it('should have expected fake patients (Observable.do)', async(inject([], () => {
+        backend.connections.subscribe((c: MockConnection) => c.mockRespond(response));
+
+        service.getPatientVariantReportOcp('fake-psn', 'faka-analysis-id')
+          .do(patient => {
+            expect(patient).toBe(fakeData, 'should have expected no. of patients');
+          })
+          .toPromise();
+      })));
+
+      it('should be OK returning no patients', async(inject([], () => {
+        let resp = new Response(new ResponseOptions({ status: 200, body: {} }));
+        backend.connections.subscribe((c: MockConnection) => c.mockRespond(resp));
+
+        service.getPatientVariantReportOcp('fake-psn', 'faka-analysis-id')
+          .do(patient => {
+            expect(patient).toEqual({}, 'should have no patients');
+          })
+          .toPromise();
+      })));
+
+      it('should NOT treat 404 as an Observable error', async(inject([], () => {
+        let resp = new Response(new ResponseOptions({ status: 404 }));
+        backend.connections.subscribe((c: MockConnection) => c.mockRespond(resp));
+
+        service.getPatientVariantReportOcp('fake-psn', 'faka-analysis-id')
+          .do(patientWitError => {
+            expect(patientWitError).toEqual({ hasError: true, error: new Error('Bad response status: 404') }, 'should have hasError=true');
+          })
+          .catch(err => {
+            fail('should not respond with patients');
+            return Observable.of(null); // failure is the expected test result
+          })
+          .toPromise();
+      })));
+
+    });
+
+
+    describe('when getPatientCopyNumberReport', () => {
+      let backend: MockBackend;
+      let service: PatientApiService;
+      let fakeData: any;
+      let response: Response;
+
+      beforeEach(inject([AuthHttp, XHRBackend], (http: AuthHttp, be: MockBackend) => {
+        backend = be;
+        service = new PatientApiService(http);
+        fakeData = makeVariantReportQcData();
+        let options = new ResponseOptions({ status: 200, body: fakeData });
+        response = new Response(options);
+      }));
+
+      it('should have expected fake patients (then)', async(inject([], () => {
+        backend.connections.subscribe((c: MockConnection) => c.mockRespond(response));
+
+        service.getPatientCopyNumberReport('fake-psn', 'faka-analysis-id').toPromise()
+          .then(patient => {
+            expect(patient).toBe(fakeData);
+          });
+      })));
+
+      it('should have expected fake patients (Observable.do)', async(inject([], () => {
+        backend.connections.subscribe((c: MockConnection) => c.mockRespond(response));
+
+        service.getPatientCopyNumberReport('fake-psn', 'faka-analysis-id')
+          .do(patient => {
+            expect(patient).toBe(fakeData, 'should have expected no. of patients');
+          })
+          .toPromise();
+      })));
+
+      it('should be OK returning no patients', async(inject([], () => {
+        let resp = new Response(new ResponseOptions({ status: 200, body: {} }));
+        backend.connections.subscribe((c: MockConnection) => c.mockRespond(resp));
+
+        service.getPatientCopyNumberReport('fake-psn', 'faka-analysis-id')
+          .do(patient => {
+            expect(patient).toEqual({}, 'should have no patients');
+          })
+          .toPromise();
+      })));
+
+      it('should NOT treat 404 as an Observable error', async(inject([], () => {
+        let resp = new Response(new ResponseOptions({ status: 404 }));
+        backend.connections.subscribe((c: MockConnection) => c.mockRespond(resp));
+
+        service.getPatientCopyNumberReport('fake-psn', 'faka-analysis-id')
+          .do(patientWitError => {
+            expect(patientWitError).toEqual(
+              { hasError: true, error: new Error('Bad response status: 404'), parsed_vcf_genes: {} }, 'should have hasError=true'
+            );
+          })
+          .catch(err => {
+            fail('should not respond with patients');
+            return Observable.of(null); // failure is the expected test result
+          })
+          .toPromise();
+      })));
+
+    });
+
   });
 }
