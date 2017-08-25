ea2c5df78eed8d2ce2c8be79911a7ca48dfaa727
Rick Zakharov <rick.zakharov@gmail.com>
2017-08-25 15:22:36 -0400

    Implementing unit tests.

--- src/client/app/patient/patient-details/patient-details.component.spec.ts
+++ src/client/app/patient/patient-details/patient-details.component.spec.ts
@@ -1,7 +1,6 @@
 import {
   Component,
   ChangeDetectorRef,
-  Injectable,
   DebugElement
 } from '@angular/core';
 import {
@@ -10,10 +9,9 @@ import {
   ComponentFixture
 } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { ActivatedRoute, convertToParamMap, ParamMap, ActivatedRouteSnapshot, Data } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { RouterTestingModule } from '@angular/router/testing';
 import { DropzoneModule } from 'ngx-dropzone-wrapper';
 
@@ -27,44 +25,10 @@ import { PatientTimelineModule } from './../patient-timeline/patient-timeline.mo
 import { SharedModule } from '../../shared/shared.module';
 
 import { PatientApiServiceStub, PatientApiServiceWithErrorStub } from '../testing/patient-api-service-stub';
-import { ViewDataTransformerStub } from '../testing/view-data-transformer-stubs';
-
-@Injectable()
-export class ActivatedRouteStub {
-  // ActivatedRoute.paramMap is Observable
-  private subject = new BehaviorSubject(convertToParamMap(this.testParamMap));
-  // tslint:disable-next-line:member-ordering
-  paramMap = this.subject.asObservable();
-  private trasformerStub: ViewDataTransformerStub = new ViewDataTransformerStub();
-
-  // Test parameters
-  private _testParamMap: ParamMap;
-  get testParamMap() { return this._testParamMap; }
-  set testParamMap(params: {}) {
-    this._testParamMap = convertToParamMap(params);
-    this.subject.next(this._testParamMap);
-  }
-
-  private actvatedSnapshot: ActivatedRouteSnapshot;
-
-  // ActivatedRoute.snapshot.paramMap
-  get snapshot(): ActivatedRouteSnapshot {
-    return this.actvatedSnapshot;
-  }
-
-  constructor() {
-    this.actvatedSnapshot = new ActivatedRouteSnapshot();
-    this.actvatedSnapshot.data = {};
-    this.actvatedSnapshot.data['data'] = this.trasformerStub.makePatientListData();
-  }
-
-  toString(): string {
-    return "ActivatedRouteStub";
-  }
-}
+import { ActivatedRouteStub } from '../testing/activated-route-stub';
 
 export function main() {
-  fdescribe('PatientDetailsComponent (templateUrl)', () => {
+  describe('PatientDetailsComponent (templateUrl)', () => {
 
     let component: PatientDetailsComponent;
     let fixture: ComponentFixture<PatientDetailsComponent>;
@@ -110,165 +74,51 @@ export function main() {
       el = de.nativeElement;
     });
 
-    fit('no PSN in title until manually call `detectChanges`', () => {
+    it('no PSN in title until manually call `detectChanges`', () => {
       expect(el.textContent).toEqual('Patient ');
     });
 
-    fit('should display PSN in the title', () => {
+    it('should display PSN in the title', () => {
       fixture.detectChanges();
-      expect(el.textContent).toEqual('Patient ' + component.psn);
+      expect(el.textContent).toEqual('Patient ' + component.patient.patientSequenceNumber);
     });
-  });
+
+    it('should work for download', () => {
+      let api = fixture.debugElement.injector.get(PatientApiService);
+      let spy = spyOn(api, 'downloadPatientFile').and.callFake(() => { ; });
+      component.download('fake_url');
+
+      expect(spy).toHaveBeenCalled();
+    });
 
 
-  describe('patient-details component', () => {
-    let fixture: ComponentFixture<PatientDetailsComponent>;
-    let component: PatientDetailsComponent;
-
-    let config: any[] = [
-      { path: 'patients/1234', component: 'PatientDetailsComponent' }
-    ];
-
-    beforeEach(() => {
-      TestBed.configureTestingModule({
-        imports: [
-          RouterTestingModule.withRoutes(config),
-          DirectivesModule,
-          PipesModule,
-          FormsModule,
-          DataTableModule,
-          PatientTimelineModule,
-          DropzoneModule,
-          SharedModule
-        ],
-        declarations: [PatientDetailsComponent],
-        providers: [
-          { provide: ActivatedRoute, useClass: ActivatedRouteStub },
-          { provide: PatientApiService, useClass: PatientApiServiceStub },
-          // { provide: ActivatedRoute, useValue: { snapshot: { params: { patientSequenceNumber: 1234 } } } },
-          ChangeDetectorRef,
-          ViewDataTransformer
-        ]
+    describe('with upload', () => {
+      beforeEach(() => {
+        component.ngOnInit();
       });
-    });
 
-    it('should work for download', async(() => {
-      TestBed
-        .compileComponents()
-        .then(() => {
-          fixture = TestBed.overrideComponent(PatientDetailsComponent, {
-            set: {
-              templateUrl: ''
-            }
-          }).createComponent(PatientDetailsComponent);
+      it('should initialize drop-zones', () => {
 
-          component = fixture.componentInstance;
-          let api = fixture.debugElement.injector.get(PatientApiService);
-
-          let spy = spyOn(api, 'downloadPatientFile').and.callFake(() => { ; });
-
-          component.download('fake_url');
-
-          expect(spy).toHaveBeenCalled();
-        });
-    }));
-
-    it('should work for ngoninit', async(() => {
-      TestBed
-        .compileComponents()
-        .then(() => {
-          fixture = TestBed.overrideComponent(PatientDetailsComponent, {
-            set: {
-              templateUrl: ''
-            }
-          }).createComponent(PatientDetailsComponent);
-
-          component = fixture.componentInstance;
-
-          component.ngOnInit();
-
-          expect(component.configVariantZip).toBeDefined();
-          expect(component.configDnaBam).toBeDefined();
-          expect(component.configCdnaBam).toBeDefined();
-          expect(component.configDocuments).toBeDefined();
-
-        });
-    }));
-
-    fit('should work for onUploadSuccess', async(() => {
-      TestBed
-        .compileComponents()
-        .then(() => {
-          fixture = TestBed.overrideComponent(PatientDetailsComponent, {
-            set: {
-              templateUrl: ''
-            }
-          }).createComponent(PatientDetailsComponent);
-          fixture.componentInstance.onUploadSuccess("test");
-        });
-    }));
-
-    xit('should work for uploadFiles', async(() => {
-      TestBed
-        .compileComponents()
-        .then(() => {
-          fixture = TestBed.overrideComponent(PatientDetailsComponent, {
-            set: {
-              templateUrl: ''
-            }
-          }).createComponent(PatientDetailsComponent);
-
-          fixture.componentInstance.configVariantZip = {};
-          fixture.componentInstance.configDnaBam = {};
-          fixture.componentInstance.configCdnaBam = {};
-
-          fixture.componentInstance.uploadFiles();
-        });
-    }));
-  });
-
-  describe('patient-details component with error', () => {
-    let fixture: ComponentFixture<PatientDetailsComponent>;
-    let component: PatientDetailsComponent;
-
-    let config: any[] = [
-      { path: 'patients/1234', component: 'PatientDetailsComponent' }
-    ];
-
-    beforeEach(() => {
-      TestBed.configureTestingModule({
-        imports: [
-          RouterTestingModule.withRoutes(config),
-          DirectivesModule,
-          PipesModule,
-          FormsModule,
-          DataTableModule,
-          PatientTimelineModule,
-          DropzoneModule,
-          SharedModule
-        ],
-        declarations: [PatientDetailsComponent],
-        providers: [
-          { provide: PatientApiService, useClass: PatientApiServiceWithErrorStub },
-          { provide: ActivatedRoute, useValue: { snapshot: { params: { patientSequenceNumber: 1234 } } } },
-          ChangeDetectorRef,
-          ViewDataTransformer
-        ]
+        expect(component.configVariantZip).toBeDefined();
+        expect(component.configDnaBam).toBeDefined();
+        expect(component.configCdnaBam).toBeDefined();
+        expect(component.configDocuments).toBeDefined();
       });
+
+      it('should call onUploadSuccess with no errors', () => {
+        expect(() => { component.onUploadSuccess('test'); }).not.toThrowError();
+      });
+
+      it('should call uploadFiles with no errors', () => {
+        expect(() => { component.uploadFiles(); }).not.toThrowError();
+
+        expect(component.configVariantZip.autoProcessQueue).toBe(true);
+        expect(component.configDnaBam.autoProcessQueue).toBe(true);
+        expect(component.configCdnaBam.autoProcessQueue).toBe(true);
+      });
+
     });
-
-    xit('should work for getData', async(() => {
-      TestBed
-        .compileComponents()
-        .then(() => {
-          fixture = TestBed.overrideComponent(PatientDetailsComponent, {
-            set: {
-              templateUrl: ''
-            }
-          }).createComponent(PatientDetailsComponent);
-        });
-    }));
-
   });
+
 }
 
--- src/client/app/patient/testing/view-data-transformer-stubs.ts
+++ src/client/app/patient/testing/view-data-transformer-stubs.ts
@@ -1,13 +1,25 @@
+import { Tabs } from '../patient-details/patient-details.module';
+
 export class ViewDataTransformerStub {
     makePatientListData = () => {
         return {
             psn: 'fake-psm',
-            patient: {},
+            patient: {
+                patientSequenceNumber: 'fake-psn',
+                disease: {
+                    name: 'fake-name',
+                    shortName: 'fake-shortName',
+                    ctepCategory: 'fake-ctepCategory',
+                    ctepSubCategory: 'fake-ctepSubCategory',
+                    ctepTerm: 'fake-ctepTerm',
+                    medDRACode: 'fake-medDRACode'
+                }
+            },
             summaryData: {},
             section: 'fake-section',
             entityId: 'fake-entity-id',
             needToScroll: () => { ; },
-            tabs: {}
+            tabs: new Tabs(),
         };
     }
 }
