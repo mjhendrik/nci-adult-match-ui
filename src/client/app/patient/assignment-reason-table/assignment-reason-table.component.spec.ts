3a5fd3f747b5fb8a20bb5e20b895d98a6146ced8
Rick Zakharov <rick.zakharov@gmail.com>
2017-08-28 14:36:57 -0400

    AssignmentReasonTableComponent unit test is at 100%

--- src/client/app/patient/assignment-reason-table/assignment-reason-table.component.spec.ts
+++ src/client/app/patient/assignment-reason-table/assignment-reason-table.component.spec.ts
@@ -1,40 +1,84 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  DebugElement
+} from '@angular/core';
 import {
   async,
-  TestBed
+  TestBed,
+  ComponentFixture
 } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
-import { AssignmentReasonTableModule } from './assignment-reason-table.module';
+import { PipesModule } from './../../shared/pipes/pipes.module';
 import { SharedModule } from '../../shared/shared.module';
-
-export function main() {
-  describe('assignment-reason-table component', () => {
-    // Setting module for testing
-    // Disable old forms
-
-    beforeEach(() => {
-      TestBed.configureTestingModule({
-        declarations: [TestComponent],
-        imports: [AssignmentReasonTableModule, SharedModule]
-      });
-    });
-
-    // it('should work',
-    //   async(() => {
-    //     TestBed
-    //       .compileComponents()
-    //       .then(() => {
-    //         let fixture = TestBed.createComponent(TestComponent);
-    //         let aboutDOMEl = fixture.debugElement.children[0].nativeElement;
-
-    //       });
-    //   }));
-
-  });
-}
+import { AssignmentReasonTableComponent, AssignmentReasonSection } from './assignment-reason-table.component';
 
 @Component({
   selector: 'test-cmp',
-  template: '<sd-assignment-reason-table></sd-assignment-reason-table>'
+  template: '<sd-assignment-reason-table [reasons]="testItems"></sd-assignment-reason-table>'
 })
-class TestComponent { }
+class TestComponent {
+  items: AssignmentReasonSection[] = [];
+}
+
+export function main() {
+  describe('AssignmentReasonTableComponent (templateUrl)', () => {
+    let hostComponent: TestComponent;
+    let fixture: ComponentFixture<TestComponent>;
+    let de: DebugElement;
+    let el: HTMLElement;
+
+    // async beforeEach
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        imports: [
+          PipesModule,
+          SharedModule
+        ],
+        declarations: [AssignmentReasonTableComponent, TestComponent]
+      }).compileComponents();  // compile template and css
+    }));
+
+    // synchronous beforeEach
+    beforeEach(() => {
+      fixture = TestBed.createComponent(TestComponent);
+      hostComponent = fixture.componentInstance;
+      de = fixture.debugElement.query(By.css('sd-assignment-reason-table'));
+      console.log(fixture.debugElement.nativeElement.innerText);
+      el = de.nativeElement;
+    });
+
+    it('should have no table body until manually calling `detectChanges`', () => {
+      let tbody = fixture.debugElement.query(By.css('tbody'));
+      expect(tbody).toBeNull();
+    });
+
+    it('should display "No Assignment data" when host provides empty array', () => {
+      fixture.detectChanges();
+      let tbody = fixture.debugElement.query(By.css('tbody'));
+      let rows = tbody.queryAll(By.css('tr'));
+      expect(rows.length).toBe(1);
+      expect((rows[0].nativeElement as HTMLElement).innerText).toContain('No Assignment data');
+    });
+
+    it('should display 2 rows when host provides array of 2 items', () => {
+      let a = new AssignmentReasonSection();
+      a.name = 'fake-section1';
+      a.items.push('fake-reason1');
+      hostComponent.items.push(a);
+
+      a = new AssignmentReasonSection();
+      a.name = 'fake-section2';
+      a.items.push('fake-reason2');
+      a.items.push('fake-reason3');
+      hostComponent.items.push(a);
+
+      fixture.detectChanges();
+      let tbody = fixture.debugElement.query(By.css('tbody'));
+      let rows = tbody.queryAll(By.css('tr'));
+      expect(rows.length).toBe(1);
+      expect((rows[0].nativeElement as HTMLElement).innerText).toContain('No Assignment data');
+    });
+
+  });
+}
