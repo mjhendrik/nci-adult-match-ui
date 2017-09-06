b3532003b8edd825a1276cf716cf511cdd11532e
Rick Zakharov <rick.zakharov@gmail.com>
2017-09-06 18:09:22 -0400

    Fixing docker build for aws

--- tools/env/dev-docker.ts
+++ tools/env/dev-docker.ts
@@ -5,7 +5,7 @@ const DevConfig: EnvConfig = {
   API: {
     PATIENT: 'http://localhost:5000/api/v1',
     TREATMENT_ARM: 'http://localhost:5010/api/v1',
-    ION_REPORTER: 'http://localhost:3001/api/v1'
+    ION_REPORTER: 'http://localhost:3002/api/v1'
   },
   CLIENT_ID: 'RjoYZXUDEzQxMJw04C6B5dsQKqUAEYzA',
   AUTH_DOMAIN: 'ncimatch.auth0.com'
