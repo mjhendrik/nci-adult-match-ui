68409bcf1119fe95363623293f12d8e5447a97dd
Rick Zakharov <rick.zakharov@gmail.com>
2017-09-21 15:56:44 -0400

    Implementing unit tests for CLIA

---
+++ src/client/app/clia/aliquot-api.service.spec.ts
@@ -0,0 +1,111 @@
+import {
+  async,
+  TestBed,
+  inject
+} from '@angular/core/testing';
+import {
+  HttpModule,
+  Http,
+  Response,
+  ResponseOptions,
+  XHRBackend
+} from '@angular/http';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import { AuthHttp } from 'angular2-jwt';
+import { Observable } from 'rxjs/Observable';
+
+import { CliaApiServiceStub } from './testing/clia-api-service-stub';
+import { AliquotApiService } from './aliquot-api.service';
+import { CliaVariantReportsNTCInterface } from './clia-data-interfaces';
+
+export function main() {
+  describe('AliquotApiService (mockBackend)', () => {
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [HttpModule],
+        providers: [
+          AliquotApiService,
+          { provide: XHRBackend, useClass: MockBackend },
+          { provide: AuthHttp, useExisting: Http },
+        ]
+      });
+    });
+
+    it('can provide the mockBackend as XHRBackend', inject([XHRBackend], (backend: MockBackend) => {
+      expect(backend).not.toBeNull('backend should be provided');
+    }));
+
+    it('can instantiate service with "new"', inject([AuthHttp], (http: AuthHttp) => {
+      expect(http).not.toBeNull('http should be provided');
+      let service = new AliquotApiService(http);
+      expect(service instanceof AliquotApiService).toBe(true, 'new service should be ok');
+    }));
+
+    it('can instantiate service when inject service', inject([AliquotApiService], (service: AliquotApiService) => {
+      expect(service instanceof AliquotApiService).toBe(true);
+    }));
+
+
+    describe('when getCliaVariantReportsNTC', () => {
+      let backend: MockBackend;
+      let service: AliquotApiService;
+      let fakeData: CliaVariantReportsNTCInterface;
+      let response: Response;
+
+      beforeEach(inject([AuthHttp, XHRBackend], (http: AuthHttp, be: MockBackend) => {
+        backend = be;
+        service = new AliquotApiService(http);
+        fakeData = CliaApiServiceStub.makeCliaVariantReportsNTCData();
+        let options = new ResponseOptions({ status: 200, body: fakeData });
+        response = new Response(options);
+      }));
+
+      it('should have expected fake data analysis_id (then)', async(inject([], () => {
+        backend.connections.subscribe((c: MockConnection) => c.mockRespond(response));
+
+        service.getCliaVariantReportsNTC('fake_molecular_id').toPromise()
+          .then(data => {
+            expect(data.analysis_id).toBe(fakeData.analysis_id, 'should have expected analysis_id');
+          });
+      })));
+
+      it('should have expected fake data analysis_id count (Observable.do)', async(inject([], () => {
+        backend.connections.subscribe((c: MockConnection) => c.mockRespond(response));
+
+        service.getCliaVariantReportsNTC('fake_molecular_id')
+          .do(data => {
+            expect(data.analysis_id).toBe(fakeData.analysis_id, 'should have expected analysis_id');
+          })
+          .toPromise();
+      })));
+
+      it('should be OK returning no data', async(inject([], () => {
+        let resp = new Response(new ResponseOptions({ status: 200, body: null }));
+        backend.connections.subscribe((c: MockConnection) => c.mockRespond(resp));
+
+        service.getCliaVariantReportsNTC('fake_molecular_id')
+          .do(data => {
+            expect(data).toBe(null, 'should have no data');
+          })
+          .toPromise();
+      })));
+
+      it('should treat 404 as an Observable error', async(inject([], () => {
+        let resp = new Response(new ResponseOptions({ status: 404 }));
+        backend.connections.subscribe((c: MockConnection) => c.mockRespond(resp));
+
+        service.getCliaVariantReportsNTC('fake_molecular_id')
+          .do(data => {
+            fail('should not respond with data');
+          })
+          .catch(err => {
+            expect(err).toMatch(/Bad response status/, 'should catch bad response status code');
+            return Observable.of(null); // failure is the expected test result
+          })
+          .toPromise();
+      })));
+    });
+
+
+  });
+}
--- src/client/app/clia/testing/clia-api-service-stub.ts
+++ src/client/app/clia/testing/clia-api-service-stub.ts
@@ -37,4 +37,31 @@ export class CliaApiServiceStub {
         'site': 'mocha',
         'study_id': 'EAY131'
     }] as any[]
+
+    static makeCliaVariantReportsNTCData = () => {
+        return {
+            'analysis_id': 'SC_KGPVI_analysis_1',
+            'cellularity': '1.000000',
+            'control_type': 'no_template',
+            'copy_number_variant_genes': null,
+            'copy_number_variants': [],
+            'date_molecular_id_created': '2016-10-28 12:56:19.566',
+            'date_variant_received': '2016-11-10 17:46:49.401014',
+            'editable': 'true',
+            'filename': 'SC_KGPVI_MoCha_12_notemplate_passed',
+            'gene_fusions': [],
+            'ion_reporter_id': 'IR_WAO85',
+            'mapd': '1.934',
+            'molecular_id': 'NTC_MOCHA_KGPVI',
+            'molecular_id_type': 'sample_control',
+            'oncomine_control_panel_summary': null,
+            'report_status': 'PASSED',
+            'site': 'mocha',
+            'snv_indels': [],
+            'torrent_variant_caller_version': '4.4-8',
+            'total_variants': 0,
+            'tsv_name': 'IR_WAO85/NTC_MOCHA_KGPVI/SC_KGPVI_analysis_1/SC_KGPVI_MoCha_12_notemplate_passed.tsv',
+            'vcf_name': 'IR_WAO85/NTC_MOCHA_KGPVI/SC_KGPVI_analysis_1/SC_KGPVI_MoCha_12_notemplate_passed.vcf'
+        } as any;
+    }
 }
