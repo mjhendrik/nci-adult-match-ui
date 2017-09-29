d5abc723bfed592f4e9f39bec1b752a0ead4fc13
mjhendrik <matti.hendriksson@nih.gov>
2017-09-29 11:19:25 -0400

    Biopsy Api specs cleaned.  TA Api specs updated.

--- src/client/app/biopsy-tracking/biopsy-tracking-api.service.spec.ts
+++ src/client/app/biopsy-tracking/biopsy-tracking-api.service.spec.ts
@@ -42,64 +42,65 @@ export function main() {
       expect(service instanceof BiopsyTrackingApiService).toBe(true);
     }));
 
-      describe('when getPatientList', () => {
-        let backend: MockBackend;
-        let service: BiopsyTrackingApiService;
-        let fakeBios: any[];
-        let response: Response;
-        let fakeCount: number;
+    describe('when Biopsy tracking data', () => {
+      let backend: MockBackend;
+      let service: BiopsyTrackingApiService;
+      let fakeBios: any[];
+      let response: Response;
+      let fakeCount: number;
 
-        beforeEach(inject([AuthHttp, XHRBackend], (http: AuthHttp, be: MockBackend) => {
-          backend = be;
-          service = new BiopsyTrackingApiService(http);
-          fakeBios = PatientApiServiceStub.makeBiopsytrackingData();
-          let options = new ResponseOptions({ status: 200, body: fakeBios });
-          response = new Response(options);
-          fakeCount = 10;
-        }));
+      beforeEach(inject([AuthHttp, XHRBackend], (http: AuthHttp, be: MockBackend) => {
+        backend = be;
+        service = new BiopsyTrackingApiService(http);
+        fakeBios = PatientApiServiceStub.makeBiopsytrackingData();
+        let options = new ResponseOptions({ status: 200, body: fakeBios });
+        response = new Response(options);
+        fakeCount = 10;
+      }));
 
-        it('should have expected tracking count (then)', async(inject([], () => {
-          backend.connections.subscribe((c: MockConnection) => c.mockRespond(response));
+      it('should have expected tracking count (then)', async(inject([], () => {
+        backend.connections.subscribe((c: MockConnection) => c.mockRespond(response));
 
-          service.getBiopsyTracking(1, 2, 'sortOrder', 'sortBy', 'filter').toPromise()
-            .then(tracking => {
-              expect(tracking.length).toEqual(fakeBios.length);
-            });
-        })));
+        service.getBiopsyTracking(1, 2, 'sortOrder', 'sortBy', 'filter').toPromise()
+          .then(tracking => {
+            expect(tracking.length).toEqual(fakeBios.length);
+          });
+      })));
 
-        it('should have expected fake bios count (then)', async(inject([], () => {
-          backend.connections.subscribe((c: MockConnection) => c.mockRespond(response));
+      it('should have expected fake bios count (then)', async(inject([], () => {
+        backend.connections.subscribe((c: MockConnection) => c.mockRespond(response));
 
-          service.getBiopsyCount('fake-search-terms').toPromise()
-            .then(count => {
-              expect(count).toBe(fakeBios);
-            });
-        })));
+        service.getBiopsyCount('fake-search-terms').toPromise()
+          .then(count => {
+            expect(count).toBe(fakeBios);
+          });
+      })));
 
-        it('should have expected fake Total bios count (then)', async(inject([], () => {
-          backend.connections.subscribe((c: MockConnection) => c.mockRespond(response));
+      it('should have expected fake Total bios count (then)', async(inject([], () => {
+        backend.connections.subscribe((c: MockConnection) => c.mockRespond(response));
 
-          service.getBiopsyTotal().toPromise()
-            .then(count => {
-              expect(count).toBe(fakeBios);
-            });
-        })));
+        service.getBiopsyTotal().toPromise()
+          .then(count => {
+            expect(count).toBe(fakeBios);
+          });
+      })));
 
-        xit('should treat 404 as an Observable error', async(inject([], () => {
-          let resp = new Response(new ResponseOptions({ status: 404 }));
-          backend.connections.subscribe((c: MockConnection) => c.mockRespond(resp));
+      xit('should treat 404 as an Observable error', async(inject([], () => {
+        let resp = new Response(new ResponseOptions({ status: 404 }));
+        backend.connections.subscribe((c: MockConnection) => c.mockRespond(resp));
 
-          service.getBiopsyTracking(1, 2, 'sortOrder', 'sortBy', 'filter')
-            .do(tracking => {
-              fail('should not respond with patients');
-            })
-            .catch(err => {
-              expect(err).toMatch(/Bad response status/, 'should catch bad response status code');
-              return Observable.of(null); // failure is the expected test result
-            })
-            .toPromise();
-        })));
+        service.getBiopsyTracking(1, 2, 'sortOrder', 'sortBy', 'filter')
+          .do(tracking => {
+            fail('should not respond with biopsy tracking data');
+          })
+          .catch(err => {
+            expect(err).toMatch(/Bad response status/, 'should catch bad response status code');
+            return Observable.of(null); // failure is the expected test result
+          })
+          .toPromise();
+      })));
 
-      });
+    });
+      
   });
 }
--- src/client/app/treatment-arm/treatment-arm-api.service.spec.ts
+++ src/client/app/treatment-arm/treatment-arm-api.service.spec.ts
@@ -14,54 +14,105 @@ import {
   XHRBackend,
   BaseRequestOptions
 } from '@angular/http';
-import { MockBackend } from '@angular/http/testing';
+import { MockBackend, MockConnection } from '@angular/http/testing';
 import { AuthHttp } from 'angular2-jwt';
+import { PatientApiServiceStub } from '../patient/testing/patient-api-service-stub';
 
 export function main() {
   describe('treatment arm api service test', () => {
-    beforeEach(() => {
+    let backend: MockBackend;
+    let service: TreatmentArmApiService;
+    let response: Response;
+
+    beforeEach(async(() => {
       TestBed.configureTestingModule({
         imports: [HttpModule],
         providers: [
-          MockBackend,
-          BaseRequestOptions,
+          TreatmentArmApiService,
           { provide: XHRBackend, useClass: MockBackend },
-          {
-            provide: Http,
-            useFactory: (backend: MockBackend, options: BaseRequestOptions) => new Http(backend, options),
-            deps: [MockBackend, BaseRequestOptions]
-          },
           { provide: AuthHttp, useExisting: Http },
-          TreatmentArmApiService
         ]
       });
-    });
+    }));
 
-    xit('should return observable',
-      inject([TreatmentArmApiService, XHRBackend],
-        (TreatmentArmApiService: TreatmentArmApiService, mockBackend: MockBackend) => {
+    it('can instantiate service with "new"', inject([AuthHttp], (http: AuthHttp) => {
+      expect(http).not.toBeNull('http should be provided');
+      let service = new TreatmentArmApiService(http);
+      expect(service instanceof TreatmentArmApiService).toBe(true, 'new service should be ok');
+    }));
 
-          const mockResponse = {
-            data: [
-              { id: 0, name: 'Data 0' },
-              { id: 1, name: 'Data 1' },
-              { id: 2, name: 'Data 2' },
-              { id: 3, name: 'Data 3' },
-            ]
-          };
+    it('can instantiate service when inject service', inject([TreatmentArmApiService], (service: TreatmentArmApiService) => {
+      expect(service instanceof TreatmentArmApiService).toBe(true);
+    }));
 
-          mockBackend.connections.subscribe((connection: any) => {
-            connection.mockRespond(new Response(new ResponseOptions({
-              body: mockResponse
-            })));
+    describe('when Biopsy arms data', () => {
+      let backend: MockBackend;
+      let service: TreatmentArmApiService;
+      let fakeTreatmenArms: any[];
+      let response: Response;
+      let fakeCount: number;
+      let arms:any;
+
+      beforeEach(inject([AuthHttp, XHRBackend], (http: AuthHttp, be: MockBackend) => {
+        backend = be;
+        service = new TreatmentArmApiService(http);
+        fakeTreatmenArms = PatientApiServiceStub.treatmentArmData();
+        let options = new ResponseOptions({ status: 200, body: fakeTreatmenArms });
+        response = new Response(options);
+        fakeCount = 10;
+      }));
+
+      it('should have expected TA details count (then)', async(inject([], () => {
+        backend.connections.subscribe((c: MockConnection) => c.mockRespond(response));
+
+        service.getTreatmentArmDetails('EAY131-Q').toPromise()
+          .then(arms => {
+            expect(arms).toEqual(fakeTreatmenArms);
           });
+      })));
 
-          TreatmentArmApiService.getTreatmentArmDetails('1234').subscribe((response: any) => { });
-          TreatmentArmApiService.getPreviousTreatmentArmDetails('1111');
-          TreatmentArmApiService.getTreatmentArmVersions('1111');
-          TreatmentArmApiService.getTreatmentArmList();
-        })
-    );
+      it('should have expected fake Tratment arms details (then)', async(inject([], () => {
+        backend.connections.subscribe((c: MockConnection) => c.mockRespond(response));
 
+        service.getPreviousTreatmentArmDetails('EAY131-Q').toPromise()
+          .then(arms => {
+            expect(arms).toBe(fakeTreatmenArms);
+          });
+      })));
+
+      it('should have expected fake Treatment Arm version (then)', async(inject([], () => {
+        backend.connections.subscribe((c: MockConnection) => c.mockRespond(response));
+
+        service.getTreatmentArmVersions('EAY131-Q').toPromise()
+          .then(arms => {
+            expect(arms[0].version).toBe("2016-05-31");
+          });
+      })));
+
+      it('should have expected fake Treatment Arm list (then)', async(inject([], () => {
+        backend.connections.subscribe((c: MockConnection) => c.mockRespond(response));
+
+        service.getTreatmentArmList().toPromise()
+          .then(arms => {
+            expect(arms).toBe(fakeTreatmenArms);
+          });
+      })));
+
+      // xit('should treat 404 as an Observable error', async(inject([], () => {
+      //   let resp = new Response(new ResponseOptions({ status: 404 }));
+      //   backend.connections.subscribe((c: MockConnection) => c.mockRespond(resp));
+      //
+      //   service.getBiopsyTracking(1, 2, 'sortOrder', 'sortBy', 'filter')
+      //     .do(arms => {
+      //       fail('should not respond with biopsy arms data');
+      //     })
+      //     .catch(err => {
+      //       expect(err).toMatch(/Bad response status/, 'should catch bad response status code');
+      //       return Observable.of(null); // failure is the expected test result
+      //     })
+      //     .toPromise();
+      // })));
+
+    });
   });
 }
