abbd481fbf4b9418b8e9f20af423b2b10874ab9f
Rick Zakharov <rick.zakharov@gmail.com>
2017-10-03 11:50:31 -0400

    Working on runtime environment cofiguration

--- tools/env/deploy.ts
+++ tools/env/deploy.ts
@@ -2,12 +2,12 @@ import { EnvConfig } from './env-config.interface';
 
 const TestConfig: EnvConfig = {
   API: {
-    PATIENT: 'I___PATIENT___I',
-    TREATMENT_ARM: 'I___TREATMENT_ARM___I',
-    ION_REPORTERS: 'I___ION_REPORTERS___I',
-    SAMPLE_CONTROLS: 'I___SAMPLE_CONTROLS___I',
-    ALIQUOT: 'I___ALIQUOT___I',
-    MESSAGE: 'I___MESSAGE___I'
+    PATIENT: 'I___PATIENT_API___I',
+    TREATMENT_ARM: 'I___TREATMENT_ARM_API___I',
+    ION_REPORTERS: 'I___ION_REPORTERS_API___I',
+    SAMPLE_CONTROLS: 'I___SAMPLE_CONTROLS_API___I',
+    ALIQUOT: 'I___ALIQUOT_API___I',
+    MESSAGE: 'I___MESSAGE_API___I'
   },
   CLIENT_ID: 'I___CLIENT_ID___I',
   AUTH_DOMAIN: 'I___AUTH_DOMAIN___I'
