d311b25fb43a4c1ab1debcb4ef940cf40df88146
Rick Zakharov <rick.zakharov@gmail.com>
2017-10-03 10:08:03 -0400

    Working on runtime environment cofiguration

--- tools/config/setenv.sh
+++ tools/config/setenv.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-sed -i -e "s/I___name___I/$ENV_NAME/g;s/I___AUTH0_DOMAIN___I/$AUTH0_DOMAIN/g;s/I___AUTH0_CLIENT_ID___I/$AUTH0_CLIENT_ID/g;s/I___loginUrl___I/$LOGIN_URL/g;s/I___envTag___I/$ENV_TAG/g" /usr/local/apache2/match/app/js/*.app.min.js
+sed -i -e "s/I___PATIENT_API___I/$PATIENT_API/g;s/I___TREATMENT_ARM_API___I/$ENT_ARM_API/g;s/I___ION_REPORTERS_API___I/$PORTERS_API/g;s/I___SAMPLE_CONTROLS_API___I/$ONTROLS_API/g;s/I___ALIQUOT_API___I/$ALIQUOT_API/g;s/I___MESSAGE_API___I/$MESSAGE_API/g;s/I___CLIENT_ID___I/$__CLIENT_ID/g;s/I___AUTH_DOMAIN___I/$AUTH_DOMAIN/g;" /usr/local/apache2/match/app/shared/config/env.config.js
