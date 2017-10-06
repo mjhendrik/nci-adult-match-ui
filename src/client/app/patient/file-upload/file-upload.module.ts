4af27b13501fabca95928656959072d7d6090ce8
Rick Zakharov <rick.zakharov@gmail.com>
2017-10-06 16:01:54 -0400

    Replacing bootstrapt modals with ngrx modals

--- src/client/app/patient/file-upload/file-upload.module.ts
+++ src/client/app/patient/file-upload/file-upload.module.ts
@@ -4,12 +4,14 @@ import { FileUploadComponent } from './file-upload.component';
 import { PipesModule } from '../../shared/pipes/pipes.module';
 import { DirectivesModule } from '../../shared/directives/directives.module';
 import { FileUploadContentComponent } from './file-upload-content.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     PipesModule,
     DirectivesModule,
+    FormsModule
   ],
   declarations: [FileUploadComponent, FileUploadContentComponent],
   exports: [FileUploadComponent, FileUploadContentComponent]
