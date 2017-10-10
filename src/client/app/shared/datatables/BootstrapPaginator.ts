a372d0eb1e16c47199ccb36c9ec3e056dace8514
Subhash Vallala <sgvallala@gmail.com>
2017-10-10 15:03:26 -0400

    Fixing datatable bugs

--- src/client/app/shared/datatables/BootstrapPaginator.ts
+++ src/client/app/shared/datatables/BootstrapPaginator.ts
@@ -21,7 +21,7 @@ import * as _ from 'lodash';
         <div>
             <div class="dataTables_info" id="data-table_info" *ngIf="p.dataLength!=0 && (searchTerm==undefined || searchTerm.length==0)">
                 Showing {{p.dataLength==0 ? 0 : (p.activePage-1)*(p.rowsOnPage)+1}} to 
-                {{ p.dataLength < (p.activePage-1)*(p.rowsOnPage)+p.rowsOnPage ? p.dataLength: (p.activePage-1)*(p.rowsOnPage)+paginationParse(p.rowsOnPage)}} of {{totalPageCount === undefined ? p.dataLength : totalPageCount}}
+                {{ p.dataLength < (p.activePage-1)*(p.rowsOnPage)+p.rowsOnPage ? p.dataLength: (p.activePage-1)*(p.rowsOnPage)+paginationParse(p.rowsOnPage)}} of {{totalPageCount === undefined ? p.dataLength : p.totalLength}}
                           entries
             </div>
             <div class="dataTables_info" id="data-table_info" *ngIf="p.totalLength!=0 && searchTerm!=undefined && searchTerm.length!=0">
