542fe13e28e49b15420d48e137bfd4176743ec05
Rick Zakharov <rick.zakharov@gmail.com>
2017-10-16 14:01:55 -0400

    Implementing check-box with confirmation for variant confirmation

--- src/client/app/shared/check-box-with-confirm/modal-dialog-with-comments.component.ts
+++ src/client/app/shared/check-box-with-confirm/modal-dialog-with-comments.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { BsModalRef } from 'ngx-bootstrap';
 
 @Component({
     selector: 'sd-modal-dialog-with-comments',
     templateUrl: 'modal-dialog-with-comments.component.html'
+//     template: `
+//     <div class="modal-header">
+//       <h4 class="modal-title pull-left">{{title}}</h4>
+//       <button type="button" class="close pull-right" aria-label="Close" (click)="bsModalRef.hide()">
+//         <span aria-hidden="true">&times;</span>
+//       </button>
+//     </div>
+//     <div class="modal-body">
+//     vidy
+//     </div>
+//     <div class="modal-footer">
+//       <button type="button" class="btn btn-default" (click)="bsModalRef.hide()">Close</button>
+//     </div>
+//   `
 })
 export class ModalDialogWithCommentsComponent {
+    constructor(public bsModalRef: BsModalRef) { }
 }
-
