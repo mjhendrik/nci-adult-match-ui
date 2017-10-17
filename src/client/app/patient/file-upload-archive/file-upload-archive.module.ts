1680448327b4b9e922587bb23d646f05ac47eb96
Subhash Vallala <sgvallala@gmail.com>
2017-10-17 13:22:41 -0400

    Removing dropzone

---
+++ src/client/app/patient/file-upload-archive/file-upload-archive.module.ts
@@ -0,0 +1,22 @@
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { FormsModule } from '@angular/forms';
+// import { DropzoneModule } from 'ngx-dropzone-wrapper';
+
+// import { PipesModule } from '../../shared/pipes/pipes.module';
+// import { DirectivesModule } from '../../shared/directives/directives.module';
+// import { FileUploadComponent } from './file-upload.component';
+// import { FileUploadContentComponent } from './file-upload-content.component';
+
+// @NgModule({
+//   imports: [
+//     CommonModule,
+//     PipesModule,
+//     DirectivesModule,
+//     FormsModule,
+//     DropzoneModule.forRoot()
+//   ],
+//   declarations: [FileUploadComponent, FileUploadContentComponent],
+//   exports: [FileUploadComponent, FileUploadContentComponent]
+// })
+// export class FileUploadModule { }
