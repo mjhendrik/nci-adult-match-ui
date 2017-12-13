fb7439cda0b1df45cd34589ceb2ac3052e2c1915
Subhash Vallala <sgvallala@gmail.com>
2017-12-13 11:54:22 -0500

    Fixed jumpy panels when clicking generate MSN button

--- src/client/app/shared/directives/match-height-tab.directive.ts
+++ src/client/app/shared/directives/match-height-tab.directive.ts
@@ -39,7 +39,7 @@ export class MatchHeightTabClickDirective {
                 .forEach((x: HTMLElement) => x.style.height = `${maxHeight}px`);
 
             setTimeout(function () {
-                window.scroll(0, 0);
+                window.dispatchEvent(new Event('resize'));
             }, 1);
 
         }, 159);
