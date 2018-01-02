f2ff2ff0c11d52ed90c344a339345076e4919043
Subhash Vallala <sgvallala@gmail.com>
2018-01-02 10:26:29 -0500

    patient-header changes

--- src/client/app/patient/patient-header/patient-header.component.ts
+++ src/client/app/patient/patient-header/patient-header.component.ts
@@ -1,14 +1,14 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  Input
+} from '@angular/core';
 
 @Component({
   moduleId: module.id,
   selector: 'sd-patient-header',
   templateUrl: 'patient-header.component.html'
 })
+
 export class PatientHeaderComponent {
   @Input() items: any;
-
-  getIcon(status: string): string {
-    return 'fa-user-o';
-  }
 }
