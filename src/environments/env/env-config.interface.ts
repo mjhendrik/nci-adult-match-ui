3fc1090cef26cd8693ce7edc4e375e1d261e6620
Rick Zakharov <rick.zakharov@gmail.com>
2018-01-03 12:10:34 -0500

    Commented out old env files

--- src/environments/env/env-config.interface.ts
+++ src/environments/env/env-config.interface.ts
@@ -1,21 +1,21 @@
-// Feel free to extend this interface
-// depending on your app specific config.
-export interface EnvConfig {
-  CLIENT_ID: string;
-  AUTH_DOMAIN: string;
-  API: EnvApiConfig;
-  ENV?: string;
-}
+// // Feel free to extend this interface
+// // depending on your app specific config.
+// export interface EnvConfig {
+//   CLIENT_ID: string;
+//   AUTH_DOMAIN: string;
+//   API: EnvApiConfig;
+//   ENV?: string;
+// }
 
-interface EnvApiConfig {
-  PATIENT: string;
-  TREATMENT_ARM: string;
-  ION_REPORTERS: string;
-  SAMPLE_CONTROLS: string;
-  ALIQUOT: string;
-  MESSAGE: string;
-}
+// interface EnvApiConfig {
+//   PATIENT: string;
+//   TREATMENT_ARM: string;
+//   ION_REPORTERS: string;
+//   SAMPLE_CONTROLS: string;
+//   ALIQUOT: string;
+//   MESSAGE: string;
+// }
 
-export class EnvConstants {
-  TBD: string = 'TBD';
-}
+// export class EnvConstants {
+//   TBD: string = 'TBD';
+// }
--- src/environments/env/prod.ts
+++ src/environments/env/prod.ts
@@ -1,16 +1,16 @@
-import { EnvConfig } from './env-config.interface';
+// import { EnvConfig } from './env-config.interface';
 
-const ProdConfig: EnvConfig = {
-  API: {
-    PATIENT: '[TBD]',
-    TREATMENT_ARM: '[TBD]',
-    ION_REPORTERS: '[TBD]',
-    SAMPLE_CONTROLS: '[TBD]',
-    ALIQUOT: '[TBD]',
-    MESSAGE: '[TBD]'
-  },
-  CLIENT_ID: '[TBD]',
-  AUTH_DOMAIN: 'ncimatch.auth0.com'
-};
+// const ProdConfig: EnvConfig = {
+//   API: {
+//     PATIENT: '[TBD]',
+//     TREATMENT_ARM: '[TBD]',
+//     ION_REPORTERS: '[TBD]',
+//     SAMPLE_CONTROLS: '[TBD]',
+//     ALIQUOT: '[TBD]',
+//     MESSAGE: '[TBD]'
+//   },
+//   CLIENT_ID: '[TBD]',
+//   AUTH_DOMAIN: 'ncimatch.auth0.com'
+// };
 
-export = ProdConfig;
+// export = ProdConfig;
